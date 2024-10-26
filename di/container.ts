3d7c6d5e36604690e011afa52553547a29095db8
jjoonleo <jjoonleo@gmail.com>
2024-10-26 16:38:10 +0900

    chore!: delete everything

--- di/container.ts
+++
@@ -1,7 +0,0 @@
-import { appSchema } from "@nozbe/watermelondb";
-import { Container } from "inversify";
-import "reflect-metadata";
-
-const container = new Container();
-
-export default container;

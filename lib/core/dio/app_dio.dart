4bd82a80b3e540bf44344a32e0acfed3783d8dc0
jjoonleo <jjoonleo@gmail.com>
2024-11-01 01:55:53 +0900

    feat: implement AppDio class for Dio instance management

---
+++ lib/core/dio/app_dio.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+
+abstract class AppDio {
+  AppDio._internal();
+
+  static Dio? _instance;
+
+  static Dio get instance => _instance ??= _AppDio();
+}
+
+class _AppDio with DioMixin implements Dio {
+  _AppDio() {
+    httpClientAdapter = IOHttpClientAdapter();
+    options = BaseOptions(
+      baseUrl: '',
+      connectTimeout: const Duration(milliseconds: 30000),
+      receiveTimeout: const Duration(milliseconds: 30000),
+      sendTimeout: const Duration(milliseconds: 30000),
+      receiveDataWhenStatusError: true,
+    );
+
+    interceptors.addAll([]);
+  }
+}

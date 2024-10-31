7e00992dd8f2f0b6e1bfdf2c792ef0ea79cf4289
[SeoHyeonSim] <[firebolt478@naver.com]>
2024-11-01 03:48:47 +0900

    chore: setup db new

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:on_time_front/config/database.dart';
-import 'package:on_time_front/data/daos/schedule_dao.dart';
 import 'package:on_time_front/domain/entities/place_entity.dart';
 import 'package:on_time_front/domain/entities/schedule_entity.dart';
+import 'package:on_time_front/domain/entities/user_entity.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -10,9 +10,17 @@ void main() async {
 
   final database = AppDatabase();
 
-  final ScheduleDao scheduleDao = ScheduleDao(database);
+  // final ScheduleDao scheduleDao = ScheduleDao(database, placeDao, userDao);
   final ScheduleEntity scheduleEntity = ScheduleEntity(
     id: 1,
+    user: UserEntity(
+        id: 1,
+        email: 'email',
+        password: 'password',
+        name: 'name',
+        spareTime: 100,
+        note: 'note',
+        score: 20),
     place: PlaceEntity(id: 1, placeName: 'placeName'),
     scheduleName: 'scheduleName',
     scheduleTime: DateTime.now(),
@@ -22,9 +30,12 @@ void main() async {
     scheduleSpareTime: DateTime.now(),
     scheduleNote: 'scheduleNote',
   );
-  scheduleDao.createSchedule(scheduleEntity);
+  database.userDao.createUser(scheduleEntity.user);
+  database.placeDao.createPlace(scheduleEntity.place);
+  database.scheduleDao.createSchedule(scheduleEntity);
 
-  final List<ScheduleEntity> scheduleList = await scheduleDao.getScheduleList();
+  final List<ScheduleEntity> scheduleList =
+      await database.scheduleDao.getScheduleList();
   debugPrint(scheduleList.first.toString());
 }
 

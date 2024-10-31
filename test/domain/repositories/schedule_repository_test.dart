050550f2d19a2d3d4e64d4b8aad3ede6720dc0e9
jjoonleo <jjoonleo@gmail.com>
2024-10-31 14:35:06 +0900

    chore: add husky and commitlint configuration files

---
+++ test/domain/repositories/schedule_repository_test.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart'; 
+import 'package:on_time_front/domain/repositories/schedule_repository.dart';
+
+void main() {
+	group(
+		ScheduleRepository, 
+		() {
+			test(
+				'',
+				() async {
+				},
+			);
+		},
+	);
+}

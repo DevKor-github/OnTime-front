635ecac86470d41d48aafd04dd6fa7e7eb7101cc
jjoonleo <jjoonleo@gmail.com>
2024-10-31 12:18:46 +0900

    fix: update main.dart to use userDao and placeDao for creating user and place entities

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:on_time_front/config/database.dart';
-import 'package:on_time_front/data/daos/schedule_dao.dart';
 import 'package:on_time_front/domain/entities/place_entity.dart';
 import 'package:on_time_front/domain/entities/schedule_entity.dart';
 import 'package:on_time_front/domain/entities/user_entity.dart';
@@ -31,7 +30,8 @@ void main() async {
     scheduleSpareTime: DateTime.now(),
     scheduleNote: 'scheduleNote',
   );
-  scheduleDao.createSchedule(scheduleEntity);
+  database.userDao.createUser(scheduleEntity.user);
+  database.placeDao.createPlace(scheduleEntity.place);
   database.scheduleDao.createSchedule(scheduleEntity);
 
   final List<ScheduleEntity> scheduleList =

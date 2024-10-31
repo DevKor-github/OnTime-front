c6059e4fcdbaaae42c86720e4bf312088d73c890
[SeoHyeonSim] <[firebolt478@naver.com]>
2024-11-01 02:38:05 +0900

    chore: db refined

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo App',
+      title: 'Flutter Demon Application',
       theme: ThemeData(
         // This is the theme of your application.
         //

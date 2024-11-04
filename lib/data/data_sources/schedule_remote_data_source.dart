53f1f1f7f0577f10eb87895dd1c25ffa29454703
jjoonleo <jjoonleo@gmail.com>
2024-11-04 15:50:49 +0900

    feat: add ScheduleRemoteDataSource interface for remote schedule management

---
+++ lib/data/data_sources/schedule_remote_data_source.dart
@@ -0,0 +1,14 @@
+import 'package:on_time_front/domain/entities/schedule_entity.dart';
+
+abstract interface class ScheduleRemoteDataSource {
+  Future<void> createSchedule(ScheduleEntity schedule);
+
+  Future<List<ScheduleEntity>> getSchedulesByDate(
+      DateTime startDate, DateTime? endDate);
+
+  Future<ScheduleEntity> getScheduleById(int id);
+
+  Future<void> updateSchedule(ScheduleEntity schedule);
+
+  Future<void> deleteSchedule(ScheduleEntity schedule);
+}

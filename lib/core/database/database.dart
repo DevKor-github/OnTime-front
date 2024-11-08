658045967718505db5f7386aeaa2f974513129ae
jjoonleo <jjoonleo@gmail.com>
2024-11-09 00:40:48 +0900

    feat: add named constructor for testing in AppDatabase

--- lib/core/database/database.dart
+++ lib/core/database/database.dart
@@ -30,6 +30,8 @@ part 'database.g.dart';
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(_openConnection());
 
+  AppDatabase.forTesting(super.e);
+
   @override
   int get schemaVersion => 3;
 

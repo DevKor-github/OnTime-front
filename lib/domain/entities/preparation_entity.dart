7412d8fc5af1c79b714982ad76e9db4e6e88d1d9
jjoonleo <jjoonleo@gmail.com>
2024-11-11 23:05:08 +0900

    feat: add methods to convert preparation steps to user and schedule models

--- lib/domain/entities/preparation_entity.dart
+++ lib/domain/entities/preparation_entity.dart
@@ -1,3 +1,4 @@
+import 'package:on_time_front/core/database/database.dart';
 import 'package:on_time_front/domain/entities/preparation_step_entity.dart';
 
 class PreparationEntity {
@@ -14,5 +15,35 @@ class PreparationEntity {
   @override
   String toString() {
     return 'PreparationEntity(preparationStepList: ${preparationStepList.toString()})';
+  }
+
+  toPreparationUserEntity(String userId) {}
+
+  List<PreparationSchedule> toPreparationScheduleModelList(String scheduleId) {
+    return preparationStepList
+        .map(
+          (e) => PreparationSchedule(
+            preparationName: e.preparationName,
+            preparationTime: e.preparationTime,
+            order: e.order,
+            scheduleId: scheduleId,
+            id: 'scheduleId',
+          ),
+        )
+        .toList();
+  }
+
+  List<PreparationUser> toPreparationUserModelList(String userId) {
+    return preparationStepList
+        .map(
+          (e) => PreparationUser(
+            preparationName: e.preparationName,
+            preparationTime: e.preparationTime,
+            order: e.order,
+            userId: userId,
+            id: 'userId',
+          ),
+        )
+        .toList();
   }
 }

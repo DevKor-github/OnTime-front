03b1a0c8a83de03aec0b87e3ce7a53cb3ade20c2
jjoonleo <jjoonleo@gmail.com>
2024-11-11 23:04:54 +0900

    refactor: remove user table reference from PreparationUsers and simplify userId column

--- lib/data/tables/preparation_user_table.dart
+++ lib/data/tables/preparation_user_table.dart
@@ -1,10 +1,9 @@
 import 'package:drift/drift.dart';
-import 'package:on_time_front/data/tables/user_table.dart';
 import 'package:uuid/uuid.dart';
 
 class PreparationUsers extends Table {
   TextColumn get id => text().clientDefault(() => Uuid().v7())();
-  TextColumn get userId => text().references(Users, #id)();
+  TextColumn get userId => text()();
   TextColumn get preparationName => text().withLength(min: 1, max: 30)();
   IntColumn get preparationTime => integer()();
   IntColumn get order => integer()();

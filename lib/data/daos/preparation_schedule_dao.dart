c2d4d760f61c586d345e9425d61eb5011f00638a
jjoonleo <jjoonleo@gmail.com>
2024-11-11 23:04:48 +0900

    refactor: streamline preparation schedule and user DAOs by removing unused entities and simplifying methods

--- lib/data/daos/preparation_schedule_dao.dart
+++ lib/data/daos/preparation_schedule_dao.dart
@@ -1,69 +1,42 @@
 import 'package:drift/drift.dart';
-import 'package:on_time_front/domain/entities/preparation_step_entity.dart';
 import '/core/database/database.dart';
 
 import 'package:on_time_front/data/tables/preparation_schedule_table.dart';
-import 'package:on_time_front/data/tables/schedules_table.dart';
-import 'package:on_time_front/data/tables/user_table.dart';
-import 'package:on_time_front/data/tables/places_table.dart';
-
-import 'package:on_time_front/domain/entities/preparation_entity.dart';
 
 part 'preparation_schedule_dao.g.dart';
 
-@DriftAccessor(tables: [PreparationSchedules, Schedules, Users, Places])
+@DriftAccessor(tables: [PreparationSchedules])
 class PreparationScheduleDao extends DatabaseAccessor<AppDatabase>
     with _$PreparationScheduleDaoMixin {
   final AppDatabase db;
 
   PreparationScheduleDao(this.db) : super(db);
 
-  Future<void> createPreparationSchedule(
-      PreparationEntity preparationEntity, String scheduleId) async {
-    for (var step in preparationEntity.preparationStepList) {
-      await into(db.preparationSchedules).insert(
-        step.toPreparationScheduleModel(scheduleId).toCompanion(false),
-      );
+  Future<List<PreparationSchedule>> createPreparationSchedule(
+      List<PreparationSchedule> preparationStepModel) async {
+    final List<PreparationSchedule> preparationScheduleList = [];
+    for (var step in preparationStepModel) {
+      preparationScheduleList
+          .add(await into(db.preparationSchedules).insertReturning(
+        step.toCompanion(false),
+      ));
     }
+    return preparationScheduleList;
   }
 
-  Future<List<PreparationEntity>> getPreparationSchedulesByScheduleId(
+  Future<List<PreparationSchedule>> getPreparationSchedulesByScheduleId(
       String scheduleId) async {
     final List<PreparationSchedule> query =
         await (select(db.preparationSchedules)
               ..where((tbl) => tbl.scheduleId.equals(scheduleId)))
             .get();
 
-    final List<PreparationStepEntity> stepEntities =
-        query.map((preparationSchedule) {
-      return PreparationStepEntity(
-        id: preparationSchedule.id,
-        preparationName: preparationSchedule.preparationName,
-        preparationTime: preparationSchedule.preparationTime,
-        order: preparationSchedule.order,
-      );
-    }).toList();
+    return query;
+  }
 
-    // await Future.forEach(
-    //   query,
-    //   (preparationSchedule) async {
-    //     final schedule = await (select(db.schedules)
-    //           ..where((tbl) => tbl.id.equals(preparationSchedule.scheduleId)))
-    //         .getSingle();
-    //     final user = await (select(db.users)
-    //           ..where((tbl) => tbl.id.equals(schedule.userId)))
-    //         .getSingle();
-    //     final place = await (select(db.places)
-    //           ..where((tbl) => tbl.id.equals(schedule.placeId)))
-    //         .getSingle();
-
-    //     preparationScheduleList.add(
-    //       PreparationEntity.fromModel(
-    //           preparationSchedule, schedule, user, place),
-    //     );
-    //   },
-    // );
-
-    return [PreparationEntity(preparationStepList: stepEntities)];
+  Future<void> deletePreparationScheduleByScheduleId(String scheduleId) async {
+    await (delete(db.preparationSchedules)
+          ..where((tbl) => tbl.scheduleId.equals(scheduleId)))
+        .go();
   }
 }
--- lib/data/daos/preparation_user_dao.dart
+++ lib/data/daos/preparation_user_dao.dart
@@ -1,63 +1,38 @@
 import 'package:drift/drift.dart';
-import 'package:on_time_front/domain/entities/preparation_step_entity.dart';
 import '/core/database/database.dart';
 import 'package:on_time_front/data/tables/preparation_user_table.dart';
-import 'package:on_time_front/data/tables/user_table.dart';
-
-import 'package:on_time_front/domain/entities/preparation_entity.dart';
 
 part 'preparation_user_dao.g.dart';
 
-@DriftAccessor(tables: [PreparationUsers, Users])
+@DriftAccessor(tables: [PreparationUsers])
 class PreparationUserDao extends DatabaseAccessor<AppDatabase>
     with _$PreparationUserDaoMixin {
   final AppDatabase db;
 
   PreparationUserDao(this.db) : super(db);
 
-  Future<void> createPreparationUser(
-      PreparationEntity preparationEntity, String userId) async {
-    for (var step in preparationEntity.preparationStepList) {
-      await into(db.preparationUsers).insert(
-        step.toPreparationUserModel(userId).toCompanion(false),
-      );
+  Future<List<PreparationUser>> createPreparationUser(
+      List<PreparationUser> preparationUserModel, String userId) async {
+    final List<PreparationUser> preparationUserList = [];
+    for (var step in preparationUserModel) {
+      preparationUserList.add(await into(db.preparationUsers).insertReturning(
+        step.toCompanion(false),
+      ));
     }
+    return preparationUserList;
   }
 
-  Future<List<PreparationEntity>> getPreparationUsersByUserId(
+  Future<List<PreparationUser>> getPreparationUsersByUserId(
       String userId) async {
     final List<PreparationUser> query = await (select(db.preparationUsers)
           ..where((tbl) => tbl.userId.equals(userId)))
         .get();
-    final List<PreparationStepEntity> stepEntities = [];
+    return query;
+  }
 
-    for (var preparationUser in query) {
-      stepEntities.add(
-        PreparationStepEntity(
-          id: preparationUser.id,
-          preparationName: preparationUser.preparationName,
-          preparationTime: preparationUser.preparationTime,
-          order: preparationUser.order,
-        ),
-      );
-    }
-
-    // await Future.forEach(
-    //   query,
-    //   (preparationUser) async {
-    //     final user = await (select(db.users)
-    //           ..where((tbl) => tbl.id.equals(preparationUser.userId)))
-    //         .getSingle();
-
-    //     preparationUserList.add(
-    //       PreparationEntity.fromModel(
-    //         preparationUser as PreparationSchedule,
-    //         user,
-    //       ),
-    //     );
-    //   },
-    // );
-
-    return [PreparationEntity(preparationStepList: stepEntities)];
+  Future<void> deletePreparationUserByUserId(String userId) async {
+    await (delete(db.preparationUsers)
+          ..where((tbl) => tbl.userId.equals(userId)))
+        .go();
   }
 }

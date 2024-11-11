78845abb61123b53ff1eb3be78dc03313a7d7820
jjoonleo <jjoonleo@gmail.com>
2024-11-11 23:05:18 +0900

    feat: implement PreparationLocalDataSource with custom preparation methods and update functionality

--- lib/data/data_sources/preparation_local_data_source.dart
+++ lib/data/data_sources/preparation_local_data_source.dart
@@ -3,12 +3,6 @@ import 'package:on_time_front/domain/entities/preparation_entity.dart';
 import 'package:on_time_front/domain/entities/preparation_step_entity.dart';
 
 abstract interface class PreparationLocalDataSource {
-  final AppDatabase appDatabase;
-
-  PreparationLocalDataSource({
-    required this.appDatabase,
-  });
-
   Future<void> createDefaultPreparation(
       PreparationEntity preparationEntity, String userId);
 
@@ -30,4 +24,69 @@ abstract interface class PreparationLocalDataSource {
 
   Future<PreparationStepEntity> getPreparationStepById(
       String preparationStepId);
+}
+
+class PreparationLocalDataSourceImpl implements PreparationLocalDataSource {
+  final AppDatabase appDatabase;
+
+  PreparationLocalDataSourceImpl({
+    required this.appDatabase,
+  });
+
+  @override
+  Future<void> createCustomPreparation(
+      PreparationEntity preparation, String scheduleId) async {
+    await appDatabase.preparationScheduleDao.createPreparationSchedule(
+        preparation.toPreparationScheduleModelList(scheduleId));
+  }
+
+  @override
+  Future<void> createDefaultPreparation(
+      PreparationEntity preparationEntity, String userId) async {
+    await appDatabase.preparationUserDao.createPreparationUser(
+        preparationEntity.toPreparationUserModelList(userId), userId);
+  }
+
+  @override
+  Future<void> deletePreparation(PreparationEntity preparationEntity) {
+    // TODO: implement deletePreparation
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<PreparationEntity> getPreparationByScheduleId(String scheduleId) {
+    // TODO: implement getPreparationByScheduleId
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<PreparationStepEntity> getPreparationStepById(
+      String preparationStepId) {
+    // TODO: implement getPreparationStepById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updatePreparationStep(
+      PreparationStepEntity preparationStepEntity) {
+    // TODO: implement updatePreparation
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateCustomPreparation(
+      PreparationEntity preparationEntity, String scheduleId) async {
+    await appDatabase.preparationScheduleDao
+        .deletePreparationScheduleByScheduleId(scheduleId);
+    await appDatabase.preparationScheduleDao.createPreparationSchedule(
+        preparationEntity.toPreparationScheduleModelList(scheduleId));
+  }
+
+  @override
+  Future<void> updateDefaultPreparation(
+      PreparationEntity preparationEntity, String userId) async {
+    await appDatabase.preparationUserDao.deletePreparationUserByUserId(userId);
+    await appDatabase.preparationUserDao.createPreparationUser(
+        preparationEntity.toPreparationUserModelList(userId), userId);
+  }
 }

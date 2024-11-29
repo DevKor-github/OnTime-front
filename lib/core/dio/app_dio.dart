cd64c67aa8d9c7d74643b907f4817dfbdfd47af8
jjoonleo <jjoonleo@gmail.com>
2024-11-29 10:02:20 +0900

    feat: update AppDio to change baseUrl and add headers for API requests

--- lib/core/dio/app_dio.dart
+++ lib/core/dio/app_dio.dart
@@ -2,24 +2,21 @@ import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:on_time_front/core/dio/interceptors/logger_interceptor.dart';
 
-abstract class AppDio {
-  AppDio._internal();
-
-  static Dio? _instance;
-
-  static Dio get instance => _instance ??= _AppDio();
-}
-
-class _AppDio with DioMixin implements Dio {
-  _AppDio() {
+class AppDio with DioMixin implements Dio {
+  AppDio() {
     httpClientAdapter = IOHttpClientAdapter();
     options = BaseOptions(
-      baseUrl: 'http://ejun.kro.kr:8000',
-      connectTimeout: const Duration(milliseconds: 30000),
-      receiveTimeout: const Duration(milliseconds: 30000),
-      sendTimeout: const Duration(milliseconds: 30000),
-      receiveDataWhenStatusError: true,
-    );
+        baseUrl: 'http://ejun.kro.kr:8888',
+        connectTimeout: const Duration(milliseconds: 30000),
+        receiveTimeout: const Duration(milliseconds: 30000),
+        sendTimeout: const Duration(milliseconds: 30000),
+        receiveDataWhenStatusError: true,
+        followRedirects: false,
+        headers: {
+          "Accept": "application/json",
+          "Authorization":
+              "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTczMjcwMDQ5NywiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIiwidXNlcklkIjoxfQ.9LtGN1omJ2LkEnFUXT4pP52BE7dOyubX25VGQkIxRY6PxCxO74fiYsUosfo_XDmI5nf4cNuNGlqYVwNSK1qWTA"
+        });
 
     interceptors.addAll([LoggerInterceptor()]);
   }

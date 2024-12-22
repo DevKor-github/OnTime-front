795ef3fb22c572a77cf0307f1258ee0e6c9c0deb
[SeoHyeonSim] <[firebolt478@naver.com]>
2024-12-22 17:55:28 +0900

    chore: change preparation usecase to use repo

--- lib/domain/use-cases/update_preparation_use_case.dart
+++ lib/domain/use-cases/update_preparation_use_case.dart
@@ -1,12 +1,12 @@
 import 'package:on_time_front/domain/entities/preparation_step_entity.dart';
-import 'package:on_time_front/data/data_sources/preparation_remote_data_source.dart';
+import 'package:on_time_front/domain/repositories/preparation_repository.dart';
 
 class UpdatePreparationUseCase {
-  final PreparationRemoteDataSource remoteDataSource;
+  final PreparationRepository _preparationRepository;
 
-  UpdatePreparationUseCase(this.remoteDataSource);
+  UpdatePreparationUseCase(this._preparationRepository);
 
   Future<void> call(PreparationStepEntity preparationStepEntity) async {
-    await remoteDataSource.updatePreparation(preparationStepEntity);
+    await _preparationRepository.updatePreparation(preparationStepEntity);
   }
 }

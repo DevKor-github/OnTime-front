36e3c4f4170072bfda2fff137256d5f5d2ba88fe
jjoonleo <jjoonleo@gmail.com>
2024-12-30 15:40:19 +0900

    refactor: move home_screen file to screen folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:on_time_front/presentation/home/home_screen.dart';
-import 'package:on_time_front/presentation/onboarding/onboarding_screen.dart';
 import 'package:on_time_front/presentation/shared/router/go_router.dart';
 import 'package:on_time_front/presentation/shared/theme/theme.dart';
 

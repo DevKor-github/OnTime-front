76e08f65e8e823ab98d788cd7037fc6fc92766b7
jjoonleo <jjoonleo@gmail.com>
2024-12-30 23:53:37 +0900

    feat: convert CalendarScreen to ConsumerStatefulWidget and integrate calendar schedules

--- lib/presentation/calendar/screens/calendar_screen.dart
+++ lib/presentation/calendar/screens/calendar_screen.dart
@@ -1,56 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:on_time_front/presentation/calendar/view_models/calendar_view_model.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class CalendarScreen extends ConsumerWidget {
+class CalendarScreen extends ConsumerStatefulWidget {
   const CalendarScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends ConsumerState<CalendarScreen> {
+  DateTime selectedDate =
+      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+
+  @override
+  Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
     final colorScheme = theme.colorScheme;
+    final schedules = ref.watch(calendarViewModelProvider(
+        DateTime(DateTime.now().year, DateTime.now().month)));
     return Scaffold(
       backgroundColor: colorScheme.surfaceContainerLowest,
       appBar: AppBar(
         title: const Text('Calendar'),
       ),
-      body: TableCalendar(
-        focusedDay: DateTime.now(),
-        firstDay: DateTime(2024, 12, 1),
-        lastDay: DateTime(2025, 12, 31),
-        calendarFormat: CalendarFormat.month,
-        headerStyle: HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-        ),
-        daysOfWeekStyle: DaysOfWeekStyle(
-          weekdayStyle: textTheme.bodySmall!,
-          weekendStyle: textTheme.bodySmall!,
-        ),
-        calendarStyle: CalendarStyle(
-          outsideDaysVisible: false,
-          weekendTextStyle: textTheme.bodySmall!,
-        ),
-        onDaySelected: (selectedDay, focusedDay) {
-          debugPrint(selectedDay.toIso8601String());
-        },
-        calendarBuilders: CalendarBuilders(
-          todayBuilder: (context, day, focusedDay) => Container(
-            margin: const EdgeInsets.all(4.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: theme.colorScheme.primary,
-              shape: BoxShape.circle,
-            ),
-            child: Text(
-              day.day.toString(),
-              style: textTheme.bodySmall?.copyWith(
-                color: theme.colorScheme.onPrimary,
+      body: Column(
+        children: [
+          switch (schedules) {
+            AsyncValue(:final error?) => Text('Error: $error'),
+            AsyncValue(:final valueOrNull?) => TableCalendar(
+                eventLoader: (day) {
+                  final datesOfSchedules = valueOrNull.map((schedule) {
+                    return schedule.scheduleTime;
+                  });
+                  return datesOfSchedules.where((schedule) {
+                    return schedule.year == day.year &&
+                        schedule.month == day.month &&
+                        schedule.day == day.day;
+                  }).toList();
+                },
+                focusedDay: DateTime.now(),
+                firstDay: DateTime(2024, 12, 1),
+                lastDay: DateTime(2025, 12, 31),
+                calendarFormat: CalendarFormat.month,
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                ),
+                daysOfWeekStyle: DaysOfWeekStyle(
+                  weekdayStyle: textTheme.bodySmall!,
+                  weekendStyle: textTheme.bodySmall!,
+                ),
+                calendarStyle: CalendarStyle(
+                  outsideDaysVisible: false,
+                  weekendTextStyle: textTheme.bodySmall!,
+                  defaultTextStyle: textTheme.bodySmall!,
+                  markerDecoration: BoxDecoration(
+                    color: colorScheme.primary,
+                    shape: BoxShape.circle,
+                  ),
+                  markerMargin: EdgeInsets.symmetric(horizontal: 1.0),
+                ),
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    selectedDate = selectedDay;
+                  });
+                  debugPrint(selectedDay.toIso8601String());
+                },
+                calendarBuilders: CalendarBuilders(
+                  todayBuilder: (context, day, focusedDay) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: theme.colorScheme.primary,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      day.day.toString(),
+                      style: textTheme.bodySmall?.copyWith(
+                        color: theme.colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
+            _ => const SizedBox(),
+          },
+          Text(selectedDate.toIso8601String()),
+        ],
       ),
     );
   }

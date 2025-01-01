fe7923dc865e0be8ee165df6f0f6e6968c8c3801
jjoonleo <jjoonleo@gmail.com>
2025-01-01 22:05:26 +0900

    feat: enhance CalendarScreen with schedule grouping and improved UI layout

--- lib/presentation/calendar/view_models/calendar_view_model.dart
+++ lib/presentation/calendar/view_models/calendar_view_model.dart
@@ -7,7 +7,56 @@ part 'calendar_view_model.g.dart';
 @riverpod
 class CalendarViewModel extends _$CalendarViewModel {
   @override
-  Future<List<ScheduleEntity>> build(DateTime date) async {
-    return await ref.watch(getSchedulesForMonthUseCaseProvider(date).future);
+  Future<Map<DateTime, List<ScheduleEntity>>> build(DateTime date) async {
+    final scheduleList =
+        await ref.watch(getSchedulesForMonthUseCaseProvider(date).future);
+    final scheduleMap = <DateTime, List<ScheduleEntity>>{};
+    //sort scheduleList by scheduleTime
+    scheduleList.sort((a, b) => a.scheduleTime.compareTo(b.scheduleTime));
+    //group scheduleList by scheduleTime
+    //ignore scheduleTime's time part
+    for (final schedule in scheduleList) {
+      final scheduleTime = DateTime(
+        schedule.scheduleTime.year,
+        schedule.scheduleTime.month,
+        schedule.scheduleTime.day,
+      );
+      if (scheduleMap.containsKey(scheduleTime)) {
+        scheduleMap[scheduleTime]!.add(schedule);
+      } else {
+        scheduleMap[scheduleTime] = [schedule];
+      }
+    }
+    return scheduleMap;
+  }
+
+  void getSchedulesForMonth(DateTime date) async {
+    final scheduleList =
+        await ref.watch(getSchedulesForMonthUseCaseProvider(date).future);
+    //add shceduleList to previous this.state
+    final scheduleMap = <DateTime, List<ScheduleEntity>>{};
+    //sort scheduleList by scheduleTime
+    scheduleList.sort((a, b) => a.scheduleTime.compareTo(b.scheduleTime));
+    //group scheduleList by scheduleTime
+    //ignore scheduleTime's time part
+    for (final schedule in scheduleList) {
+      final scheduleTime = DateTime(
+        schedule.scheduleTime.year,
+        schedule.scheduleTime.month,
+        schedule.scheduleTime.day,
+      );
+      if (scheduleMap.containsKey(scheduleTime)) {
+        scheduleMap[scheduleTime]!.add(schedule);
+      } else {
+        scheduleMap[scheduleTime] = [schedule];
+      }
+    }
+
+    await future;
+
+    state = AsyncData({
+      ...state.maybeMap(orElse: () => {}, data: (data) => data.value),
+      ...scheduleMap,
+    });
   }
 }

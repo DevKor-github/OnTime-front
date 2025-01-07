f807b632f80512be977514fdd79ae302ab9eba38
jjoonleo <jjoonleo@gmail.com>
2025-01-08 02:13:43 +0900

    feat:  route for ScheduleCreateScreen

--- lib/presentation/shared/router/go_router.dart
+++ lib/presentation/shared/router/go_router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:on_time_front/presentation/calendar/screens/calendar_screen.dart';
 import 'package:on_time_front/presentation/home/screens/home_screen.dart';
 import 'package:on_time_front/presentation/onboarding/onboarding_screen.dart';
+import 'package:on_time_front/presentation/schedule_create/screens/schedule_create_screen.dart';
 
 final goRouterConfig = GoRouter(
   initialLocation: '/home',
@@ -15,5 +16,8 @@ final goRouterConfig = GoRouter(
       builder: (context, state) => HomeScreen(),
     ),
     GoRoute(path: '/calendar', builder: (context, state) => CalendarScreen()),
+    GoRoute(
+        path: '/scheduleCreate',
+        builder: (context, state) => ScheduleCreateScreen()),
   ],
 );

4a1207b0e5185ef610963a675695c1abe62f35d4
jjoonleo <jjoonleo@gmail.com>
2025-01-19 00:52:58 +0900

    feat: add assets package and update dependencies

--- widgetbook/lib/reorderable_list.dart
+++ widgetbook/lib/reorderable_list.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:on_time_front/presentation/onboarding/onboarding_screen.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 import 'package:on_time_front/presentation/onboarding/preparation_reordarable_list.dart';
 
 @widgetbook.UseCase(name: 'Default', type: PreparationReorderableList)
 Widget buildTileUseCase(BuildContext context) {
-  return const Scaffold(body: PreparationReorderableList());
+  return Scaffold(
+      body: PreparationReorderableList(
+    preparationOrderingList: [
+      PreparationStepWithOriginalIndex(
+        originalIndex: 0,
+        preparationStep: PreparationStepWithNameAndId(
+          id: '1',
+          preparationName: 'Preparation 1',
+        ),
+      ),
+    ],
+    onReorder: (int oldIndex, int newIndex) {},
+  ));
 }

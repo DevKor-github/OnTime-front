f48e0b633a74f2e2fee5141e6ae38a9cd2ffd5af
jjoonleo <jjoonleo@gmail.com>
2025-01-31 00:06:18 +0900

    feat: update schedule forms to use new state management and event handling

--- lib/presentation/schedule_create/screens/schedule_name_form.dart
+++ lib/presentation/schedule_create/screens/schedule_name_form.dart
@@ -1,39 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:on_time_front/presentation/onboarding/mutly_page_form.dart';
-import 'package:on_time_front/presentation/schedule_create/screens/schedule_create_screen.dart';
+import 'package:on_time_front/presentation/schedule_create/bloc/schedule_form/schedule_form_bloc.dart';
 
 class ScheduleNameForm extends StatefulWidget {
   const ScheduleNameForm(
       {super.key,
       required this.formKey,
       required this.initalValue,
-      this.onSaved});
+      required this.onScheduleNameSaved});
 
   final GlobalKey<FormState> formKey;
-  final ScheduleFormData initalValue;
-  final Function(ScheduleFormData)? onSaved;
+  final ScheduleFormState initalValue;
+  final ValueChanged<String> onScheduleNameSaved;
 
   @override
   State<ScheduleNameForm> createState() => _ScheduleNameFormState();
 }
 
 class _ScheduleNameFormState extends State<ScheduleNameForm> {
-  ScheduleFormData _scheduleFormData = ScheduleFormData();
-
   @override
   Widget build(BuildContext context) {
-    return MultiPageFormField(
+    return Form(
       key: widget.formKey,
-      onSaved: () {
-        widget.onSaved?.call(_scheduleFormData);
-      },
       child: TextFormField(
         initialValue: widget.initalValue.scheduleName,
         decoration: InputDecoration(labelText: '약속 이름'),
         textInputAction: TextInputAction.done,
         onSaved: (newValue) {
-          _scheduleFormData =
-              _scheduleFormData.copyWith(scheduleName: newValue!);
+          widget.onScheduleNameSaved(newValue!);
         },
       ),
     );

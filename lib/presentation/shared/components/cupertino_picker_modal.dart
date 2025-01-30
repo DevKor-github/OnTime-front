377dc70eb1b965b1a78d2acd492bca52152e69ad
jjoonleo <jjoonleo@gmail.com>
2025-01-31 02:11:00 +0900

    feat: add showCupertinoTimerPickerModal for customizable timer selection

--- lib/presentation/shared/components/cupertino_picker_modal.dart
+++ lib/presentation/shared/components/cupertino_picker_modal.dart
@@ -2,7 +2,81 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 extension ModalBottomSheetExtension on BuildContext {
-  void showCupertinoTimePickerModal({
+  void showCupertinoTimerPickerModal({
+    required String title,
+    required BuildContext context,
+    required Duration initialValue,
+    required CupertinoTimerPickerMode mode,
+    required Function(Duration value) onSaved,
+    Function? onDisposed,
+  }) {
+    showModalBottomSheet<void>(
+      isDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        final textTheme = Theme.of(context).textTheme;
+        Duration duration = initialValue;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 29.0, vertical: 28.0),
+          child: SizedBox(
+            height: 334,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: textTheme.titleMedium),
+                Expanded(
+                  child: Center(
+                    child: CupertinoTimerPicker(
+                      mode: mode,
+                      initialTimerDuration: initialValue,
+                      itemExtent: 32,
+                      onTimerDurationChanged: (value) => duration = value,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            //calling order matters
+                            Navigator.pop(context);
+                            onDisposed?.call();
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(
+                                Color.fromARGB(255, 220, 227, 255)),
+                            foregroundColor: WidgetStatePropertyAll(
+                                Color.fromARGB(255, 92, 121, 251)),
+                          ),
+                          child: Text('취소')),
+                    ),
+                    SizedBox(width: 20.0),
+                    Expanded(
+                      flex: 1,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          onSaved(duration);
+                          onDisposed?.call();
+                        },
+                        child: Text('확인'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void showCupertinoMinutePickerModal({
     required String title,
     required BuildContext context,
     required Duration initialValue,

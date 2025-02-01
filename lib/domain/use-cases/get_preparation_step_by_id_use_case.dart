3ca8835bcd7a86d3db64dfd77b99ef750f394d14
jjoonleo <jjoonleo@gmail.com>
2025-02-01 13:48:23 +0900

    refactor: add @Injectable annotation to use case classes for dependency injection

--- lib/domain/use-cases/get_preparation_step_by_id_use_case.dart
+++ lib/domain/use-cases/get_preparation_step_by_id_use_case.dart
@@ -1,6 +1,8 @@
+import 'package:injectable/injectable.dart';
 import 'package:on_time_front/domain/entities/preparation_step_entity.dart';
 import 'package:on_time_front/domain/repositories/preparation_repository.dart';
 
+@Injectable()
 class GetPreparationStepByIdUseCase {
   final PreparationRepository _preparationRepository;
 

652f7d813107575016772362ce729c901fc65c62
jjoonleo <jjoonleo@gmail.com>
2025-02-03 03:54:21 +0900

    refactor: rename updateSchedule endpoint to updateSchedule/modify and add updatePreparationByScheduleId endpoint

--- lib/core/constants/endpoint.dart
+++ lib/core/constants/endpoint.dart
@@ -6,15 +6,14 @@ class Endpoint {
   static const _getSchedulesByDate = '/schedule/show';
 
   static const _createSchedule = '/schedule/add';
-  static const _updateSchedule = '/schedule/update';
+  static const _updateSchedule = '/schedule/modify';
   static const _deleteSchedule = '/schedule/delete';
 
   static getSchedule(String scheduleId) => '$_getScheduleById$scheduleId';
   static get getSchedulesByDate => _getSchedulesByDate;
 
   static get createSchedule => _createSchedule;
-
-  static updateSchedule(String scheduleId) => '$_updateSchedule/$scheduleId';
+  static get updateSchedule => _updateSchedule;
   static deleteSchedule(String scheduleId) => '$_deleteSchedule/$scheduleId';
 
   // preparation
@@ -35,6 +34,9 @@ class Endpoint {
   static const _updateDefaultPreparation =
       '/preparationuser/modify'; // 사용자 준비과정 수정
 
+  static const _updatePreparationByScheduleId =
+      '/preparationschedule/modify'; // 스케줄별 준비과정 수정
+
   // delelte는 api가 없음.
 //   delete는 api로 요청을 보내는 게 아니라, 전부 없애고 다시 순서를 조정하는 형태로
 // - 1 2 3 -> 2 삭제 -> 3이 2로 바뀌면서 재배치
@@ -52,4 +54,7 @@ class Endpoint {
   static get getDefaultPreparation => _getDefaultPreparation;
 
   static get updateDefaultPreparation => _updateDefaultPreparation;
+
+  static updatePreparationByScheduleId(String preparationId) =>
+      '$_updatePreparationByScheduleId/$preparationId';
 }

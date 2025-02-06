2c9d6859d7517a592229a0fe799b3682c028faeb
jjoonleo <jjoonleo@gmail.com>
2025-02-07 02:48:31 +0900

    feat: implement AppBloc with user subscription and logout functionality

---
+++ lib/presentation/app/bloc/app_event.dart
@@ -0,0 +1,13 @@
+part of 'app_bloc.dart';
+
+abstract class AppEvent {
+  const AppEvent();
+}
+
+final class AppUserSubscriptionRequested extends AppEvent {
+  const AppUserSubscriptionRequested();
+}
+
+final class AppLogoutPressed extends AppEvent {
+  const AppLogoutPressed();
+}

9f9d0751e26341bf938819ee3234e0f3db09efa8
jjoonleo <jjoonleo@gmail.com>
2025-02-07 02:48:43 +0900

    feat: refactor go_router to use AppBloc for authentication-based redirection

--- lib/presentation/shared/router/go_router.dart
+++ lib/presentation/shared/router/go_router.dart
@@ -1,34 +1,53 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:on_time_front/domain/entities/preparation_entity.dart';
+import 'package:on_time_front/presentation/app/bloc/app_bloc.dart';
 import 'package:on_time_front/presentation/calendar/screens/calendar_screen.dart';
 import 'package:on_time_front/presentation/home/screens/home_screen.dart';
 import 'package:on_time_front/presentation/onboarding/onboarding_screen.dart';
 import 'package:on_time_front/presentation/schedule_create/compoenent/preparation_edit_form.dart';
 import 'package:on_time_front/presentation/schedule_create/screens/schedule_create_screen.dart';
 import 'package:on_time_front/presentation/schedule_create/screens/schedule_edit_screen.dart';
+import 'package:on_time_front/presentation/shared/utils/stream_to_listenable.dart';
 
-final goRouterConfig = GoRouter(
-  initialLocation: '/home',
-  routes: [
-    GoRoute(
-      path: '/onboarding',
-      builder: (context, state) => OnboardingScreen(),
-    ),
-    GoRoute(
-      path: '/home',
-      builder: (context, state) => HomeScreen(),
-    ),
-    GoRoute(path: '/calendar', builder: (context, state) => CalendarScreen()),
-    GoRoute(
-        path: '/scheduleCreate',
-        builder: (context, state) => ScheduleCreateScreen()),
-    GoRoute(
-        path: '/scheduleEdit/:scheduleId',
-        builder: (context, state) => ScheduleEditScreen(
-            scheduleId: state.pathParameters['scheduleId']!)),
-    GoRoute(
-        path: '/preparationEdit',
-        builder: (context, state) => PreparationEditForm(
-            preparationEntity: state.extra as PreparationEntity)),
-  ],
-);
+GoRouter goRouterConfig(AppBloc bloc) {
+  return GoRouter(
+    refreshListenable: StreamToListenable([bloc.stream]),
+    redirect: (BuildContext context, GoRouterState state) {
+      final status = bloc.state.status;
+
+      switch (status) {
+        case AppStatus.unauthenticated:
+          return '/signIn';
+        case AppStatus.authenticated:
+          return '/home';
+        case AppStatus.onboardingNotCompleted:
+          return '/onboarding';
+      }
+    },
+    initialLocation: '/home',
+    routes: [
+      GoRoute(
+        path: '/onboarding',
+        builder: (context, state) => OnboardingScreen(),
+      ),
+      GoRoute(
+        path: '/home',
+        builder: (context, state) => HomeScreen(),
+      ),
+      GoRoute(path: '/calendar', builder: (context, state) => CalendarScreen()),
+      GoRoute(
+          path: '/scheduleCreate',
+          builder: (context, state) => ScheduleCreateScreen()),
+      GoRoute(
+          path: '/scheduleEdit/:scheduleId',
+          builder: (context, state) => ScheduleEditScreen(
+              scheduleId: state.pathParameters['scheduleId']!)),
+      GoRoute(
+          path: '/preparationEdit',
+          builder: (context, state) => PreparationEditForm(
+              preparationEntity: state.extra as PreparationEntity)),
+    ],
+  );
+}

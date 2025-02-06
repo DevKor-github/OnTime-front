889db2fb86cf060386fb7b27854d7ea7dfd8971d
jjoonleo <jjoonleo@gmail.com>
2025-02-07 02:47:40 +0900

    feat: add AuthenticationRepository interface with user stream and authentication methods

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -1,1 +1,12 @@
+import 'package:on_time_front/domain/entities/user_entity.dart';
 
+abstract interface class AuthenticationRepository {
+  Stream<UserEntity> get userStream;
+
+  Future<void> signUp(
+      {required String email, required String password, required String name});
+
+  Future<void> signIn({required String email, required String password});
+
+  Future<void> signOut();
+}

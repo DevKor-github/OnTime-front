912f8cd31128aa419ab94cadd72ee307e3885192
jjoonleo <jjoonleo@gmail.com>
2025-02-08 00:49:34 +0900

    feat: add Test widget to demonstrate Google sign-in functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:on_time_front/core/di/di_setup.dart';
+import 'package:on_time_front/domain/repositories/authentication_repository.dart';
 import 'package:on_time_front/presentation/app/screens/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
   runApp(App());
+}
+
+class Test extends StatelessWidget {
+  Test({Key? key}) : super(key: key);
+
+  final AuthenticationRepository _authenticationRepository =
+      getIt.get<AuthenticationRepository>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              _authenticationRepository.signInWithGoogle();
+            },
+            child: const Text('Hello, World!'),
+          ),
+        ),
+      ),
+    );
+  }
 }

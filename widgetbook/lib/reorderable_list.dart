1b00d5b86385ab304c830801c9668da7897d1df2
jjoonleo <jjoonleo@gmail.com>
2025-02-10 20:50:59 +0900

    chore: comment out unused buildTileUseCase in reorderable_list.dart

--- widgetbook/lib/reorderable_list.dart
+++ widgetbook/lib/reorderable_list.dart
@@ -3,19 +3,19 @@ import 'package:on_time_front/presentation/onboarding/components/preparation_reo
 import 'package:on_time_front/presentation/onboarding/screens/onboarding_screen.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
-@widgetbook.UseCase(name: 'Default', type: PreparationReorderableList)
-Widget buildTileUseCase(BuildContext context) {
-  return Scaffold(
-      body: PreparationReorderableList(
-    preparationOrderingList: [
-      PreparationStepWithOriginalIndex(
-        originalIndex: 0,
-        preparationStep: PreparationStepWithNameAndId(
-          id: '1',
-          preparationName: 'Preparation 1',
-        ),
-      ),
-    ],
-    onReorder: (int oldIndex, int newIndex) {},
-  ));
-}
+// @widgetbook.UseCase(name: 'Default', type: PreparationReorderableList)
+// Widget buildTileUseCase(BuildContext context) {
+//   return Scaffold(
+//       body: PreparationReorderableList(
+//     preparationOrderingList: [
+//       PreparationStepWithOriginalIndex(
+//         originalIndex: 0,
+//         preparationStep: PreparationStepWithNameAndId(
+//           id: '1',
+//           preparationName: 'Preparation 1',
+//         ),
+//       ),
+//     ],
+//     onReorder: (int oldIndex, int newIndex) {},
+//   ));
+// }

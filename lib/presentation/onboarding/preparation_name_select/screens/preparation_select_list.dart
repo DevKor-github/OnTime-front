b61e05e8743f2cdc25c29f0360c963829c725ccf
jjoonleo <jjoonleo@gmail.com>
2025-02-18 21:28:59 +0900

    feat: refactor PreparationSelectList to use SingleChildScrollView for better layout handling

--- lib/presentation/onboarding/preparation_name_select/screens/preparation_select_list.dart
+++ lib/presentation/onboarding/preparation_name_select/screens/preparation_select_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:keyboard_avoider/keyboard_avoider.dart';
 import 'package:on_time_front/presentation/onboarding/components/onboarding_page_view_layout.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_name_select/cubit/preparation_name/preparation_name_cubit.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_name_select/cubit/preparation_step_name/preparation_step_name_cubit.dart';
@@ -18,83 +19,85 @@ class PreparationSelectList extends StatelessWidget {
 
     return BlocBuilder<PreparationNameCubit, PreparationNameState>(
         builder: (context, state) {
-      return ListView(
-        children: [
-          ListView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: state.preparationStepList.length,
-            itemBuilder: (context, index) {
-              return PreparationNameSelectField(
-                preparationStep: state.preparationStepList[index],
-                onNameChanged: (value) {
-                  context
-                      .read<PreparationNameCubit>()
-                      .preparationStepNameChanged(index: index, value: value);
-                },
-                onSelectionChanged: () {
-                  context
-                      .read<PreparationNameCubit>()
-                      .preparationStepSelectionChanged(index: index);
-                },
-              );
-            },
-          ),
-          state.status == PreparationNameStatus.adding
-              ? BlocProvider<PreparationStepNameCubit>(
-                  create: (context) => PreparationStepNameCubit(
-                      PreparationStepNameState(),
-                      preparationNameCubit:
-                          context.read<PreparationNameCubit>()),
-                  child: BlocBuilder<PreparationStepNameCubit,
-                      PreparationStepNameState>(builder: (context, state) {
-                    return PreparationNameSelectField(
-                      isAdding: true,
-                      preparationStep: state,
-                      onNameChanged: (value) {
-                        context
-                            .read<PreparationStepNameCubit>()
-                            .nameChanged(value);
-                      },
-                      onSelectionChanged: () {
-                        context
-                            .read<PreparationStepNameCubit>()
-                            .selectionToggled();
-                      },
-                      onNameSaved: () {
-                        context
-                            .read<PreparationStepNameCubit>()
-                            .preparationStepSaved();
-                      },
-                    );
-                  }),
-                )
-              : SizedBox.shrink(),
-          SizedBox(
-            height: 32.0,
-          ),
-          Center(
-            child: SizedBox(
-              height: 30,
-              width: 30,
-              child: IconButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(
-                      colorScheme.primaryContainer),
+      return SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: state.preparationStepList.length,
+              itemBuilder: (context, index) {
+                return PreparationNameSelectField(
+                  preparationStep: state.preparationStepList[index],
+                  onNameChanged: (value) {
+                    context
+                        .read<PreparationNameCubit>()
+                        .preparationStepNameChanged(index: index, value: value);
+                  },
+                  onSelectionChanged: () {
+                    context
+                        .read<PreparationNameCubit>()
+                        .preparationStepSelectionChanged(index: index);
+                  },
+                );
+              },
+            ),
+            state.status == PreparationNameStatus.adding
+                ? BlocProvider<PreparationStepNameCubit>(
+                    create: (context) => PreparationStepNameCubit(
+                        PreparationStepNameState(),
+                        preparationNameCubit:
+                            context.read<PreparationNameCubit>()),
+                    child: BlocBuilder<PreparationStepNameCubit,
+                        PreparationStepNameState>(builder: (context, state) {
+                      return PreparationNameSelectField(
+                        isAdding: true,
+                        preparationStep: state,
+                        onNameChanged: (value) {
+                          context
+                              .read<PreparationStepNameCubit>()
+                              .nameChanged(value);
+                        },
+                        onSelectionChanged: () {
+                          context
+                              .read<PreparationStepNameCubit>()
+                              .selectionToggled();
+                        },
+                        onNameSaved: () {
+                          context
+                              .read<PreparationStepNameCubit>()
+                              .preparationStepSaved();
+                        },
+                      );
+                    }),
+                  )
+                : SizedBox.shrink(),
+            SizedBox(
+              height: 28.0,
+            ),
+            Center(
+              child: SizedBox(
+                height: 30,
+                width: 30,
+                child: IconButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all<Color>(
+                        colorScheme.primaryContainer),
+                  ),
+                  onPressed: () {
+                    context
+                        .read<PreparationNameCubit>()
+                        .preparationStepCreateRequested();
+                  },
+                  color: colorScheme.onPrimary,
+                  icon: Icon(Icons.add),
+                  padding: EdgeInsets.zero,
+                  iconSize: 30.0,
                 ),
-                onPressed: () {
-                  context
-                      .read<PreparationNameCubit>()
-                      .preparationStepCreateRequested();
-                },
-                color: colorScheme.onPrimary,
-                icon: Icon(Icons.add),
-                padding: EdgeInsets.zero,
-                iconSize: 30.0,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     });
   }
@@ -146,34 +149,37 @@ class _PreparationNameSelectFieldState
     if (widget.isAdding) {
       focusNode.requestFocus();
     }
-    return Tile(
-      key: ValueKey<String>(widget.preparationStep.preparationId),
-      style: TileStyle(padding: EdgeInsets.all(16.0)),
-      leading: SizedBox(
-          width: 30,
-          height: 30,
-          child: CheckButton(
-            isChecked: widget.preparationStep.isSelected,
-            onPressed: widget.onSelectionChanged,
-          )),
-      child: Expanded(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 19.0),
-          child: Container(
-            constraints: BoxConstraints(minHeight: 30),
-            child: TextFormField(
-              initialValue: widget.preparationStep.preparationName.value,
-              onChanged: widget.onNameChanged,
-              onFieldSubmitted: (value) => widget.onNameSaved?.call(),
-              onTapOutside: (event) {
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-              decoration: InputDecoration(
-                isDense: true,
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(3.0),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Tile(
+        key: ValueKey<String>(widget.preparationStep.preparationId),
+        style: TileStyle(padding: EdgeInsets.all(16.0)),
+        leading: SizedBox(
+            width: 30,
+            height: 30,
+            child: CheckButton(
+              isChecked: widget.preparationStep.isSelected,
+              onPressed: widget.onSelectionChanged,
+            )),
+        child: Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 19.0),
+            child: Container(
+              constraints: BoxConstraints(minHeight: 30),
+              child: TextFormField(
+                initialValue: widget.preparationStep.preparationName.value,
+                onChanged: widget.onNameChanged,
+                onFieldSubmitted: (value) => widget.onNameSaved?.call(),
+                onTapOutside: (event) {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
+                decoration: InputDecoration(
+                  isDense: true,
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(3.0),
+                ),
+                focusNode: focusNode,
               ),
-              focusNode: focusNode,
             ),
           ),
         ),

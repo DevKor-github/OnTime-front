4cc86a885607b6e6bfdb1a2762c22593f45769b1
jjoonleo <jjoonleo@gmail.com>
2025-02-24 16:03:06 +0900

    feat: refactor PreparationTimeInputFieldList to use preparationTimeList and onPreparationTimeChanged callback

--- lib/presentation/onboarding/preparation_time/screens/preparation_time_input_list.dart
+++ lib/presentation/onboarding/preparation_time/screens/preparation_time_input_list.dart
@@ -10,10 +10,12 @@ import 'package:on_time_front/presentation/shared/theme/tile_style.dart';
 class PreparationTimeInputFieldList extends StatefulWidget {
   const PreparationTimeInputFieldList({
     super.key,
-    required this.formKey,
+    required this.preparationTimeList,
+    required this.onPreparationTimeChanged,
   });
 
-  final GlobalKey<FormState> formKey;
+  final List<PreparationStepTimeState> preparationTimeList;
+  final Function(int index, Duration value) onPreparationTimeChanged;
 
   @override
   State<PreparationTimeInputFieldList> createState() =>
@@ -22,6 +24,59 @@ class PreparationTimeInputFieldList extends StatefulWidget {
 
 class _PreparationTimeInputFieldListState
     extends State<PreparationTimeInputFieldList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.preparationTimeList.length,
+      itemBuilder: (context, index) {
+        final value = widget.preparationTimeList[index];
+        return Tile(
+          style: TileStyle(
+              margin: EdgeInsets.only(bottom: 8),
+              backgroundColor: Color(0xFFE6E9F9)),
+          trailing: Row(
+            children: [
+              GestureDetector(
+                child: Text(
+                    (value.preparationTime.value.inMinutes < 10 ? '0' : '') +
+                        value.preparationTime.value.inMinutes.toString()),
+                onTap: () {
+                  context.showCupertinoMinutePickerModal(
+                    title: '시간을 선택해주세요',
+                    initialValue: value.preparationTime.value,
+                    onSaved: (value) {
+                      widget.onPreparationTimeChanged(index, value);
+                    },
+                  );
+                },
+              ),
+              SizedBox(width: 35),
+              Text('분'),
+            ],
+          ),
+          child: Text(value.preparationName),
+        );
+      },
+    );
+  }
+}
+
+class PreparationTimeField extends StatefulWidget {
+  const PreparationTimeField({super.key, required this.formKey});
+
+  final GlobalKey<FormState> formKey;
+
+  @override
+  State<PreparationTimeField> createState() => _PreparationTimeFieldState();
+}
+
+class _PreparationTimeFieldState extends State<PreparationTimeField> {
+  @override
+  void initState() {
+    context.read<PreparationTimeCubit>().initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -35,38 +90,12 @@ class _PreparationTimeInputFieldListState
         onSaved: () {},
         child: BlocBuilder<PreparationTimeCubit, PreparationTimeState>(
           builder: (context, state) {
-            return ListView.builder(
-              itemCount: state.preparationTimeList.length,
-              itemBuilder: (context, index) {
-                final value = state.preparationTimeList[index];
-                return Tile(
-                  style: TileStyle(
-                      margin: EdgeInsets.only(bottom: 8),
-                      backgroundColor: Color(0xFFE6E9F9)),
-                  trailing: Row(
-                    children: [
-                      GestureDetector(
-                        child: Text(
-                            (value.preparationTime.inMinutes < 10 ? '0' : '') +
-                                value.preparationTime.inMinutes.toString()),
-                        onTap: () {
-                          context.showCupertinoMinutePickerModal(
-                            title: '시간을 선택해주세요',
-                            initialValue: value.preparationTime,
-                            onSaved: (value) {
-                              context
-                                  .read<PreparationTimeCubit>()
-                                  .preparationTimeChanged(index, value);
-                            },
-                          );
-                        },
-                      ),
-                      SizedBox(width: 35),
-                      Text('분'),
-                    ],
-                  ),
-                  child: Text(value.preparationName),
-                );
+            return PreparationTimeInputFieldList(
+              preparationTimeList: state.preparationTimeList,
+              onPreparationTimeChanged: (index, value) {
+                context
+                    .read<PreparationTimeCubit>()
+                    .preparationTimeChanged(index, value);
               },
             );
           },

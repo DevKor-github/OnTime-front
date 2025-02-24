81e1f155b5a1945e23ea45fa3f1a8d7c0a993400
jjoonleo <jjoonleo@gmail.com>
2025-02-25 00:38:47 +0900

    refactor: separate ScheduleSpareTimeField and ScheduleSpareTimeForm into different files

--- lib/presentation/onboarding/screens/onboarding_screen.dart
+++ lib/presentation/onboarding/screens/onboarding_screen.dart
@@ -9,7 +9,7 @@ import 'package:on_time_front/presentation/onboarding/preparation_name_select/sc
 import 'package:on_time_front/presentation/onboarding/preparation_time/cubit/preparation_time_cubit.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_time/screens/preparation_time_form.dart';
 import 'package:on_time_front/presentation/onboarding/schedule_spare_time/cubit/schedule_spare_time_cubit.dart';
-import 'package:on_time_front/presentation/onboarding/schedule_spare_time/screens/schedule_spare_time_input.dart';
+import 'package:on_time_front/presentation/onboarding/schedule_spare_time/screens/schedule_spare_time_form.dart';
 import 'package:on_time_front/presentation/onboarding/cubit/onboarding_cubit.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_order/cubit/preparation_order_cubit.dart';
 

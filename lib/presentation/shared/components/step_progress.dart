c7e611d77e7e82feb7bebdf88b731498e16469b7
jjoonleo <jjoonleo@gmail.com>
2025-02-27 00:57:53 +0900

    feat: enhance StepProgress component with dynamic step handling and improved layout

--- lib/presentation/shared/components/step_progress.dart
+++ lib/presentation/shared/components/step_progress.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class StepProgress extends StatelessWidget {
-  const StepProgress({super.key, required this.tabController});
+  const StepProgress(
+      {super.key,
+      required this.currentStep,
+      required this.totalSteps,
+      this.singleLine = false});
 
-  final TabController tabController;
+  final int currentStep;
+  final int totalSteps;
+
+  /// If true, the step string will be displayed in a single line
+  final bool singleLine;
+
+  final double circleRadius = 11;
+  final double lineWidth = 57;
 
   Color _getIndicatorColor(BuildContext context, int index) {
     final colorScheme = Theme.of(context).colorScheme;
-    if (tabController.index >= index) {
+    if (currentStep >= index) {
       return colorScheme.primary;
     } else {
       return colorScheme.outlineVariant;
@@ -17,67 +28,143 @@ class StepProgress extends StatelessWidget {
   List<Widget> _buildIndicator(BuildContext context, int index) {
     return [
       if (index != 0)
-        Flexible(
-          child: Container(
-            height: 2,
-            color: _getIndicatorColor(context, index),
-            constraints: BoxConstraints(maxWidth: 57.0, minWidth: 10.0),
-          ),
+        _IndicatorLine(
+          color: _getIndicatorColor(context, index),
+          lineWidth: lineWidth,
         ),
       Padding(
-        padding: const EdgeInsets.all(5.5),
-        child: Container(
-          width: 11,
-          height: 11,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(
-              color: _getIndicatorColor(context, index),
-              width: 1.5,
-            ),
-            color: index != tabController.index
-                ? _getIndicatorColor(context, index)
-                : Colors.transparent,
-          ),
-        ),
+        padding: EdgeInsets.all(circleRadius / 2),
+        child: _IndicatorCircle(
+            color: _getIndicatorColor(context, index),
+            radius: circleRadius,
+            filled: index != currentStep),
       ),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
+    final stepTextStyle = TextStyle(
+      fontSize: 14.0,
+    );
+
     return ConstrainedBox(
       constraints: BoxConstraints(
-        maxWidth: tabController.length * (57.0 + 11 + 11) - 57.0 + 10.0,
+        maxWidth: totalSteps * (lineWidth + circleRadius + circleRadius) -
+            lineWidth +
+            10.0,
       ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                for (int i = 0; i < tabController.length; i++)
-                  ..._buildIndicator(context, i),
-              ],
-            ),
+          LayoutBuilder(
+            builder: (context, constraints) {
+              final textPainter = TextPainter(
+                text: TextSpan(
+                  text: 'STEP${singleLine ? ' ' : '\n'}1',
+                  style: stepTextStyle,
+                ),
+                textDirection: TextDirection.ltr,
+              )..layout();
+              final textWidth = textPainter.size.width;
+
+              return Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: textWidth / 2 - circleRadius),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (int i = 0; i < totalSteps; i++)
+                      ..._buildIndicator(context, i),
+                  ],
+                ),
+              );
+            },
           ),
+          SizedBox(height: 8),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              for (int i = 0; i < tabController.length; i++)
-                Text(
-                  'step\n${i + 1}',
-                  style: TextStyle(
+              for (int i = 0; i < totalSteps; i++)
+                _StepText(
+                  step: i + 1,
+                  singleLine: singleLine,
+                  style: stepTextStyle.copyWith(
                     color: _getIndicatorColor(context, i),
                   ),
-                  textAlign: TextAlign.center,
-                ),
+                )
             ],
           )
         ],
+      ),
+    );
+  }
+}
+
+class _StepText extends StatelessWidget {
+  const _StepText({
+    required this.step,
+    required this.singleLine,
+    this.style,
+  });
+
+  final int step;
+  final bool singleLine;
+  final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'STEP${singleLine ? ' ' : '\n'}$step',
+      textAlign: TextAlign.center,
+      style: style,
+    );
+  }
+}
+
+class _IndicatorLine extends StatelessWidget {
+  const _IndicatorLine({required this.color, required this.lineWidth});
+
+  final Color color;
+  final double lineWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        height: 2,
+        color: color,
+        constraints: BoxConstraints(maxWidth: lineWidth, minWidth: 10.0),
+      ),
+    );
+  }
+}
+
+class _IndicatorCircle extends StatelessWidget {
+  const _IndicatorCircle({
+    required this.color,
+    required this.radius,
+    required this.filled,
+  });
+
+  final Color color;
+  final double radius;
+  final bool filled;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: radius,
+      height: radius,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: color,
+          width: 1.5,
+        ),
+        color: filled ? color : Colors.transparent,
       ),
     );
   }

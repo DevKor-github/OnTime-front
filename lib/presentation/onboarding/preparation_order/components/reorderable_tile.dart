9d565ee56db0d0cdb36d06fce7b755a90f02b8ca
jjoonleo <jjoonleo@gmail.com>
2025-02-27 22:47:37 +0900

    feat: improve layout and styling in PreparationNameSelectField and ReorderableTile components

--- lib/presentation/onboarding/preparation_order/components/reorderable_tile.dart
+++ lib/presentation/onboarding/preparation_order/components/reorderable_tile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_order/cubit/preparation_order_cubit.dart';
@@ -23,16 +24,44 @@ class ReorderableTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
     return Tile(
       style: TileStyle(
         backgroundColor: Color(0xFFE6E9F9),
+        padding: EdgeInsets.all(18.0),
+      ),
+      leading: Container(
+        height: 22,
+        width: 22,
+        decoration: BoxDecoration(
+          color: colorScheme.primary,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Center(
+          child: Text(
+            (index + 1).toString(),
+            style: TextStyle(
+              color: colorScheme.onPrimary,
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
       ),
       trailing: ReorderableDragStartListener(
         index: index,
         child: dragIndicatorSvg,
       ),
-      child: Text(
-        preparationStepOrderState.preparationName,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Text(
+          preparationStepOrderState.preparationName,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: colorScheme.onPrimaryContainer,
+          ),
+        ),
       ),
     );
   }

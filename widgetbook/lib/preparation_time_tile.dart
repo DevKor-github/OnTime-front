8a03f95f90c57192aeb65664e1bd983356983622
jjoonleo <jjoonleo@gmail.com>
2025-02-28 01:33:27 +0900

    feat: update PreparationTimeTile and add Widgetbook use case for PreparationTimeInputFieldList

--- widgetbook/lib/preparation_time_tile.dart
+++ widgetbook/lib/preparation_time_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_time/components/preparation_time_tild.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_time/cubit/preparation_time_cubit.dart';
+import 'package:on_time_front/presentation/onboarding/preparation_time/input_models/preparation_time_input_model.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
@@ -13,10 +14,21 @@ Widget useCasePreparationTimeTile(BuildContext context) {
     label: 'Preparation Name',
     initialValue: '샤워하기',
   );
-  return PreparationTimeTile(
-    value: PreparationStepTimeState(
-        preparationId: 'preparationId', preparationName: preparationName),
-    index: 0,
-    onPreparationTimeChanged: (_, __) {},
+
+  final preparationMinute = context.knobs.int.input(
+    label: 'Preparation Time',
+    initialValue: 30,
+  );
+
+  return IgnorePointer(
+    child: PreparationTimeTile(
+      value: PreparationStepTimeState(
+          preparationId: 'preparationId',
+          preparationName: preparationName,
+          preparationTime: PreparationTimeInputModel.dirty(
+              Duration(minutes: preparationMinute))),
+      index: 0,
+      onPreparationTimeChanged: (_, __) {},
+    ),
   );
 }

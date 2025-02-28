538aa55f318414e4d2b76deb97e06ea6c7d95484
jjoonleo <jjoonleo@gmail.com>
2025-02-28 16:37:48 +0900

    feat: implement PreparationCreateList and update PreparationNameForm to use it

---
+++ lib/presentation/onboarding/preparation_name_select/components/preparation_create_list.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:on_time_front/presentation/onboarding/preparation_name_select/components/preparation_name_select_field.dart';
+import 'package:on_time_front/presentation/onboarding/preparation_name_select/components/preparation_select_list.dart';
+import 'package:on_time_front/presentation/onboarding/preparation_name_select/cubit/preparation_name/preparation_name_cubit.dart';
+import 'package:on_time_front/presentation/onboarding/preparation_name_select/cubit/preparation_step_name/preparation_step_name_cubit.dart';
+
+class PreparationCreateList extends StatelessWidget {
+  const PreparationCreateList(
+      {super.key,
+      required this.preparationNameState,
+      required this.onNameChanged,
+      required this.onSelectionChanged,
+      required this.onCreationRequested});
+
+  final PreparationNameState preparationNameState;
+  final void Function({required int index, required String value})
+      onNameChanged;
+  final void Function({required int index}) onSelectionChanged;
+  final VoidCallback onCreationRequested;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          PreparationSelectList(
+            preparationStepList: preparationNameState.preparationStepList,
+            onNameChanged: (index, value) {
+              onNameChanged(index: index, value: value);
+            },
+            onSelectionChanged: (index) {
+              onSelectionChanged(index: index);
+            },
+          ),
+          preparationNameState.status == PreparationNameStatus.adding
+              ? BlocProvider<PreparationStepNameCubit>(
+                  create: (context) => PreparationStepNameCubit(
+                      PreparationStepNameState(),
+                      preparationNameCubit:
+                          context.read<PreparationNameCubit>()),
+                  child: BlocBuilder<PreparationStepNameCubit,
+                      PreparationStepNameState>(builder: (context, state) {
+                    return PreparationNameSelectField(
+                      isAdding: true,
+                      preparationStep: state,
+                      onNameChanged: (value) {
+                        context
+                            .read<PreparationStepNameCubit>()
+                            .nameChanged(value);
+                      },
+                      onSelectionChanged: () {
+                        context
+                            .read<PreparationStepNameCubit>()
+                            .selectionToggled();
+                      },
+                      onNameSaved: () {
+                        context
+                            .read<PreparationStepNameCubit>()
+                            .preparationStepSaved();
+                      },
+                    );
+                  }),
+                )
+              : SizedBox.shrink(),
+          SizedBox(
+            height: 28.0,
+          ),
+          Center(
+            child: SizedBox(
+              height: 30,
+              width: 30,
+              child: IconButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(
+                      colorScheme.primaryContainer),
+                ),
+                onPressed: () {
+                  onCreationRequested();
+                },
+                color: colorScheme.onPrimary,
+                icon: Icon(Icons.add),
+                padding: EdgeInsets.zero,
+                iconSize: 30.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 56.0),
+          SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/onboarding/preparation_name_select/screens/preparation_name_form.dart
+++ lib/presentation/onboarding/preparation_name_select/screens/preparation_name_form.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:on_time_front/presentation/onboarding/components/onboarding_page_view_layout.dart';
-import 'package:on_time_front/presentation/onboarding/preparation_name_select/components/preparation_select_list.dart';
+import 'package:on_time_front/presentation/onboarding/preparation_name_select/components/preparation_create_list.dart';
 import 'package:on_time_front/presentation/onboarding/preparation_name_select/cubit/preparation_name/preparation_name_cubit.dart';
 
 class PreparationNameForm extends StatefulWidget {
@@ -28,7 +28,19 @@ class _PreparationNameFormState extends State<PreparationNameForm> {
         '주로 하는 준비 과정을\n선택해주세요',
         style: textTheme.titleLarge,
       ),
-      child: PreparationSelectList(),
+      child: BlocBuilder<PreparationNameCubit, PreparationNameState>(
+        builder: (context, state) => PreparationCreateList(
+          preparationNameState: state,
+          onCreationRequested: context
+              .read<PreparationNameCubit>()
+              .preparationStepCreationRequested,
+          onNameChanged:
+              context.read<PreparationNameCubit>().preparationStepNameChanged,
+          onSelectionChanged: context
+              .read<PreparationNameCubit>()
+              .preparationStepSelectionChanged,
+        ),
+      ),
     );
   }
 }

e40d7e4c7f7ebc17be0ab0762c627a859311280a
jjoonleo <jjoonleo@gmail.com>
2025-03-01 23:19:44 +0900

    refactor: update onboarding title to use custom text theme and AppColors for consistency

--- lib/presentation/onboarding/components/onboarding_title.dart
+++ lib/presentation/onboarding/components/onboarding_title.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:on_time_front/presentation/shared/constants/app_colors.dart';
+import 'package:on_time_front/presentation/shared/theme/custom_text_theme.dart';
 
 class OnboardingTitle extends StatelessWidget {
   const OnboardingTitle({
@@ -13,7 +15,7 @@ class OnboardingTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
@@ -21,15 +23,13 @@ class OnboardingTitle extends StatelessWidget {
         RichText(
             text: TextSpan(
                 text: title,
-                style: Theme.of(context).textTheme.titleLarge,
+                style: textTheme.custom.titleLarge,
                 children: hint != null
                     ? [
                         TextSpan(
                           text: hint,
-                          style: TextStyle(
-                            color: colorScheme.outline,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
+                          style: textTheme.custom.bodyExtraLarge?.copyWith(
+                            color: AppColors.grey.shade600,
                           ),
                         )
                       ]

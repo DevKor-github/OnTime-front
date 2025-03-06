a3e54e2c78a2df5fe140a45e5f007f8315fae77c
jjoonleo <jjoonleo@gmail.com>
2025-03-06 22:04:54 +0900

    chore: remove unused import from onboarding_title.dart

--- widgetbook/lib/onboarding_title.dart
+++ widgetbook/lib/onboarding_title.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:on_time_front/presentation/shared/theme/custom_text_theme.dart';
 import 'package:on_time_front/presentation/onboarding/components/onboarding_title.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;

b2d42802cdeedf87a001fde5a3616a07c48cca13
jjoonleo <jjoonleo@gmail.com>
2025-03-06 22:05:49 +0900

    feat: update Firebase initialization for web support and clean up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:on_time_front/core/di/di_setup.dart';
+import 'package:on_time_front/firebase_options.dart';
 import 'package:on_time_front/presentation/app/screens/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyB61_R9KigUpSsriTYFzYCPVVjDRJs8mFU",
-        authDomain: "ontime-c63f1.firebaseapp.com",
-        projectId: "ontime-c63f1",
-        storageBucket: "ontime-c63f1.firebasestorage.app",
-        messagingSenderId: "456571312261",
-        appId: "1:456571312261:web:1d7c24d90acdc27d7e71ec",
-        measurementId: "G-4TNCHRK7KR"),
-  );
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   FirebaseMessaging messaging = FirebaseMessaging.instance;
   messaging.requestPermission(
     alert: true,
@@ -27,8 +26,8 @@ void main() async {
     provisional: true,
     sound: true,
   );
-  NotificationSettings settings = await messaging.getNotificationSettings();
-  final token = await messaging.getToken();
-  debugPrint('Token: $token');
+  // NotificationSettings settings = await messaging.getNotificationSettings();
+  // final token = await messaging.getToken();
+  // debugPrint('Token: $token');
   runApp(App());
 }

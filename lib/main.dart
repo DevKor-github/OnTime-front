0d0fd05ca27f00fe19254a246d0b5724802a2d36
jjoonleo <jjoonleo@gmail.com>
2025-03-09 14:58:29 +0900

    fix: add flutter_local_notifications dependency, configure entitlements for push notifications, and update iOS project settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:on_time_front/core/constants/environment_variable.dart';
@@ -18,17 +17,5 @@ void main() async {
   } else {
     await Firebase.initializeApp();
   }
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: true,
-    sound: true,
-  );
-  final token = await messaging.getToken();
-  debugPrint('Token: $token');
   runApp(App());
 }

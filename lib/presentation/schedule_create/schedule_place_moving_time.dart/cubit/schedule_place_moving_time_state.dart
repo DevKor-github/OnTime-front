18d09f8f4212d2510d44f1d2d15198295b75ea82
jjoonleo <jjoonleo@gmail.com>
2025-03-11 21:14:42 +0900

    feat: add SchedulePlaceMovingTimeState for managing place and time inputs in scheduling

---
+++ lib/presentation/schedule_create/schedule_place_moving_time.dart/cubit/schedule_place_moving_time_state.dart
@@ -0,0 +1,36 @@
+part of 'schedule_place_moving_time_cubit.dart';
+
+@freezed
+class SchedulePlaceMovingTimeState extends Equatable {
+  const SchedulePlaceMovingTimeState({
+    this.placeName = const SchedulePlaceInputModel.pure(),
+    this.moveTime = const ScheduleMovingTimeInputModel.pure(),
+  });
+
+  final SchedulePlaceInputModel placeName;
+  final ScheduleMovingTimeInputModel moveTime;
+
+  bool get isValid => Formz.validate([placeName, moveTime]);
+
+  SchedulePlaceMovingTimeState copyWith({
+    SchedulePlaceInputModel? placeName,
+    ScheduleMovingTimeInputModel? moveTime,
+  }) {
+    return SchedulePlaceMovingTimeState(
+      placeName: placeName ?? this.placeName,
+      moveTime: moveTime ?? this.moveTime,
+    );
+  }
+
+  static SchedulePlaceMovingTimeState fromScheduleFormState(
+      ScheduleFormState state) {
+    return SchedulePlaceMovingTimeState(
+      placeName: SchedulePlaceInputModel.pure(state.placeName ?? ''),
+      moveTime:
+          ScheduleMovingTimeInputModel.pure(state.moveTime ?? Duration.zero),
+    );
+  }
+
+  @override
+  List<Object> get props => [placeName, moveTime];
+}

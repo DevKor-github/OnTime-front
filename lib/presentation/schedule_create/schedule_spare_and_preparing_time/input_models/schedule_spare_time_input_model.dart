87ae02879269571a6135302e18764ee4e22e1b96
jjoonleo <jjoonleo@gmail.com>
2025-03-12 16:05:09 +0900

    feat: enhance ScheduleSpareTimeInputModel validation to handle null and empty values

--- lib/presentation/schedule_create/schedule_spare_and_preparing_time/input_models/schedule_spare_time_input_model.dart
+++ lib/presentation/schedule_create/schedule_spare_and_preparing_time/input_models/schedule_spare_time_input_model.dart
@@ -3,17 +3,21 @@ import 'package:formz/formz.dart';
 /// Validation errors for the [ScheduleMovingTimeInputModel] [FormzInput].
 enum ScheduleSpareTimeValidationError {
   zero,
+  empty,
 }
 
 class ScheduleSpareTimeInputModel
-    extends FormzInput<Duration, ScheduleSpareTimeValidationError> {
-  const ScheduleSpareTimeInputModel.pure([super.value = Duration.zero])
-      : super.pure();
-  const ScheduleSpareTimeInputModel.dirty([super.value = Duration.zero])
-      : super.dirty();
+    extends FormzInput<Duration?, ScheduleSpareTimeValidationError> {
+  const ScheduleSpareTimeInputModel.pure([super.value]) : super.pure();
+  const ScheduleSpareTimeInputModel.dirty([super.value]) : super.dirty();
 
   @override
-  ScheduleSpareTimeValidationError? validator(Duration value) {
-    return value.inMinutes == 0 ? ScheduleSpareTimeValidationError.zero : null;
+  ScheduleSpareTimeValidationError? validator(Duration? value) {
+    if (value == null) {
+      return ScheduleSpareTimeValidationError.empty;
+    } else if (value.inMinutes == 0) {
+      return ScheduleSpareTimeValidationError.zero;
+    }
+    return null;
   }
 }

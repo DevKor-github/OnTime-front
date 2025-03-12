760fce719717152eca730b9112807d7b0f9114fe
jjoonleo <jjoonleo@gmail.com>
2025-03-12 21:43:14 +0900

    feat: enhance PreparationFormReorderableList with reordering and time change functionality

--- lib/presentation/schedule_create/schedule_spare_and_preparing_time/preparation_form/components/preparation_form_reorderable_list.dart
+++ lib/presentation/schedule_create/schedule_spare_and_preparing_time/preparation_form/components/preparation_form_reorderable_list.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:on_time_front/presentation/schedule_create/schedule_spare_and_preparing_time/preparation_form/components/preparation_form_list_field.dart';
 import 'package:on_time_front/presentation/schedule_create/schedule_spare_and_preparing_time/preparation_form/cubit/preparation_step_form_cubit.dart';
 
@@ -7,26 +7,55 @@ class PreparationFormReorderableList extends StatelessWidget {
     super.key,
     required this.preparationStepList,
     required this.onNameChanged,
+    required this.onTimeChanged,
+    required this.onReorder,
   });
 
   final List<PreparationStepFormState> preparationStepList;
   final Function(int index, String value) onNameChanged;
+  final Function(int index, Duration value) onTimeChanged;
+  final Function(int oldIndex, int newIndex) onReorder;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      padding: EdgeInsets.zero,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: preparationStepList.length,
-      itemBuilder: (context, index) {
-        return PreparationFormListField(
-          preparationStep: preparationStepList[index],
-          onNameChanged: (value) {
-            onNameChanged(index, value);
-          },
-        );
-      },
+    Widget proxyDecorator(
+        Widget child, int index, Animation<double> animation) {
+      return AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget? child) {
+          return SizedBox(
+            child: child,
+          );
+        },
+        child: child,
+      );
+    }
+
+    return SingleChildScrollView(
+      child: ReorderableListView.builder(
+        buildDefaultDragHandles: false,
+        proxyDecorator: proxyDecorator,
+        shrinkWrap: true,
+        padding: EdgeInsets.zero,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: preparationStepList.length,
+        itemBuilder: (context, index) {
+          return PreparationFormListField(
+            key: ValueKey<String>(preparationStepList[index].id),
+            index: index,
+            preparationStep: preparationStepList[index],
+            onNameChanged: (value) {
+              onNameChanged(index, value);
+            },
+            onPreparationTimeChanged: (value) {
+              onTimeChanged(index, value);
+            },
+          );
+        },
+        onReorder: (int oldIndex, int newIndex) {
+          onReorder(oldIndex, newIndex);
+        },
+      ),
     );
   }
 }

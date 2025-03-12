533cfd69b68621fcf9aa1bba3d2a0aa4cfa9d3c7
jjoonleo <jjoonleo@gmail.com>
2025-03-12 15:55:08 +0900

    feat: add ScheduleSpareTimeCubit for managing spare time input and state updates

---
+++ lib/presentation/schedule_create/schedule_spare_and_preparing_time/cubit/schedule_spare_time_cubit.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:on_time_front/presentation/schedule_create/bloc/schedule_form/schedule_form_bloc.dart';
+import 'package:on_time_front/presentation/schedule_create/schedule_spare_and_preparing_time/input_models/schedule_spare_time_input_model.dart';
+
+part 'schedule_spare_time_state.dart';
+
+class ScheduleSpareTimeCubit extends Cubit<ScheduleSpareTimeState> {
+  ScheduleSpareTimeCubit({
+    required this.scheduleFormBloc,
+  }) : super(ScheduleSpareTimeState());
+
+  final ScheduleFormBloc scheduleFormBloc;
+
+  void initialize() {
+    final schedulePlaceMovingTimeState =
+        ScheduleSpareTimeState.fromScheduleFormState(scheduleFormBloc.state);
+    emit(state.copyWith(
+      spareTime: schedulePlaceMovingTimeState.spareTime,
+    ));
+    scheduleFormBloc.add(ScheduleFormValidated(isValid: state.isValid));
+  }
+
+  void spareTimeChanged(Duration value) {
+    final spareTime = ScheduleSpareTimeInputModel.dirty(value);
+    emit(state.copyWith(
+      spareTime: spareTime,
+    ));
+    scheduleFormBloc.add(ScheduleFormValidated(isValid: state.isValid));
+  }
+}

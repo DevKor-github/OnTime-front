ff69c79527b02ee000ca33e8b4551f9b492ee34b
jjoonleo <jjoonleo@gmail.com>
2025-03-13 17:48:06 +0900

    fix: correct import path for schedule_multi_page_form component

--- lib/presentation/schedule_create/screens/schedule_create_screen.dart
+++ lib/presentation/schedule_create/screens/schedule_create_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:on_time_front/core/di/di_setup.dart';
 import 'package:on_time_front/presentation/schedule_create/bloc/schedule_form_bloc.dart';
-import 'package:on_time_front/presentation/schedule_create/compoenent/schedule_multi_page_form.dart';
+import 'package:on_time_front/presentation/schedule_create/components/schedule_multi_page_form.dart';
 
 class ScheduleCreateScreen extends StatelessWidget {
   const ScheduleCreateScreen({super.key});

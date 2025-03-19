9b2786665572565782de8938fcded149622f3576
jjoonleo <jjoonleo@gmail.com>
2025-03-19 14:21:17 +0900

    feat: add platform-specific Google Sign-In button components for mobile and web

---
+++ lib/presentation/login/components/google_sign_in_button/google_sign_in_button_mobile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:on_time_front/core/di/di_setup.dart';
+import 'package:on_time_front/domain/repositories/user_repository.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  GoogleSignInButton({super.key});
+
+  final Widget googleIconSvg = SvgPicture.asset(
+    'assets/google_icon.svg',
+    semanticsLabel: 'Google Icon',
+    fit: BoxFit.contain,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final UserRepository authenticationRepository = getIt.get<UserRepository>();
+    return GestureDetector(
+      onTap: () async {
+        try {
+          final googleAccout =
+              await authenticationRepository.googleSignIn.signIn();
+          if (googleAccout == null) {
+            throw Exception('Google Sign In Failed, Sign In Accout is null');
+          }
+          await authenticationRepository.signInWithGoogle(googleAccout);
+        } catch (e) {
+          debugPrint(e.toString());
+        }
+      },
+      child: Container(
+        width: 44,
+        height: 44,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Color(0xFF747775)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 2,
+              offset: Offset(0, 1),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(9.0),
+          child: googleIconSvg,
+        ),
+      ),
+    );
+  }
+}

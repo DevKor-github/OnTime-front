ebd9afc20759dae7a9266e2ef576000d32a7b73f
jjoonleo <jjoonleo@gmail.com>
2025-03-21 15:49:54 +0900

    feat: implement Bottom Navigation Bar with SVG icons for Home and My

---
+++ lib/presentation/shared/components/bottom_nav_bar_scaffold.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:on_time_front/presentation/shared/constants/app_colors.dart';
+
+class BottomNavBarScaffold extends StatefulWidget {
+  const BottomNavBarScaffold({super.key, required this.child});
+  final Widget child;
+
+  @override
+  State<BottomNavBarScaffold> createState() =>
+      _BottomNavigationBarScaffoldState();
+}
+
+class _BottomNavigationBarScaffoldState extends State<BottomNavBarScaffold> {
+  int selectedIndex = 0;
+
+  final homeIcon = SvgPicture.asset(
+    'assets/Home.svg',
+    semanticsLabel: 'home',
+    height: 24,
+    fit: BoxFit.contain,
+  );
+
+  final myIcon = SvgPicture.asset(
+    'assets/My.svg',
+    semanticsLabel: 'my',
+    height: 24,
+    fit: BoxFit.contain,
+  );
+
+  final plusIcon = SvgPicture.asset(
+    'assets/Plus.svg',
+    semanticsLabel: 'plus',
+    height: 50,
+    fit: BoxFit.contain,
+  );
+
+  void onDestinationSelected(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+    switch (index) {
+      case 0:
+        context.go('/home');
+        break;
+      case 1:
+        context.go('/myPage');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      backgroundColor: AppColors.blue[100],
+      body: widget.child,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
+        ),
+        child: Container(
+          color: AppColors.white,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                  icon: _HomeIcon(),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: _MyIcon(),
+                  label: 'My',
+                ),
+              ],
+              currentIndex: selectedIndex,
+              onTap: onDestinationSelected,
+              iconSize: 24.0,
+              unselectedItemColor: colorScheme.onPrimaryContainer,
+              elevation: 0,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(top: 24.0),
+        child: SizedBox(
+          height: 76,
+          width: 76,
+          child: FilledButton(
+            onPressed: () {
+              context.go('/scheduleCreate');
+            },
+            style: FilledButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(13),
+            ),
+            child: plusIcon,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomeIcon extends StatelessWidget {
+  const _HomeIcon();
+
+  @override
+  Widget build(BuildContext context) {
+    final iconTheme = IconTheme.of(context);
+    return SvgPicture.asset(
+      'assets/Home.svg',
+      semanticsLabel: 'home',
+      height: iconTheme.size,
+      fit: BoxFit.contain,
+      colorFilter: ColorFilter.mode(
+        iconTheme.color!,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}
+
+class _MyIcon extends StatelessWidget {
+  const _MyIcon();
+
+  @override
+  Widget build(BuildContext context) {
+    final iconTheme = IconTheme.of(context);
+    return SvgPicture.asset(
+      'assets/My.svg',
+      semanticsLabel: 'myPage',
+      height: iconTheme.size,
+      fit: BoxFit.contain,
+      colorFilter: ColorFilter.mode(
+        iconTheme.color!,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}

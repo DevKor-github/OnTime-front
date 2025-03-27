d156d0c1c6e37ebd8171c1f6e8181b8211267a00
SeoHyeonSim <90302917+SeoHyeonSim@users.noreply.github.com>
2025-03-28 05:23:35 +0900

    chore: add preparation step list widget into widgetbook

---
+++ widgetbook/lib/preparatoin_step_list_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:on_time_front/domain/entities/preparation_step_entity.dart';
+import 'package:on_time_front/presentation/alarm/components/preparation_step_list_widget.dart';
+import 'package:on_time_front/presentation/shared/constants/constants.dart';
+import 'package:uuid/uuid.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(
+  name: 'Default',
+  type: PreparationStepListWidget,
+)
+Widget preparationStepListWidgetUseCase(BuildContext context) {
+  final currentStepIndex = context.knobs.int.slider(
+    label: 'Current Step Index',
+    initialValue: 0,
+    min: 0,
+    max: 1,
+  );
+
+  return SizedBox(
+    height: 400,
+    child: PreparationStepListWidget(
+      preparationSteps: [
+        PreparationStepEntity(
+          id: const Uuid().v7(),
+          preparationName: '양치하기',
+          preparationTime: const Duration(seconds: 60),
+          nextPreparationId: const Uuid().v7(),
+        ),
+        PreparationStepEntity(
+          id: const Uuid().v7(),
+          preparationName: '가방 챙기기',
+          preparationTime: const Duration(seconds: 60),
+          nextPreparationId: const Uuid().v7(),
+        ),
+      ],
+      currentStepIndex: currentStepIndex,
+      stepElapsedTimes: const [15, 0],
+      preparationStepStates: const [
+        PreparationStateEnum.now,
+        PreparationStateEnum.yet,
+      ],
+      onSkip: () {},
+    ),
+  );
+}

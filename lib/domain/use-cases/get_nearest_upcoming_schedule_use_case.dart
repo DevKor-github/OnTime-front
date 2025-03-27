faed2a6627ce7ed17ce3d1bf75fd51f858b12f38
jjoonleo <jjoonleo@gmail.com>
2025-03-27 16:30:46 +0900

    feat: enhance GetNearestUpcomingScheduleUseCase to include LoadSchedulesForWeekUseCase and streamline upcoming schedule retrieval

--- lib/domain/use-cases/get_nearest_upcoming_schedule_use_case.dart
+++ lib/domain/use-cases/get_nearest_upcoming_schedule_use_case.dart
@@ -1,40 +1,41 @@
 import 'dart:async';
 
 import 'package:injectable/injectable.dart';
-import 'package:on_time_front/data/tables/schedule_with_place_model.dart';
-import 'package:on_time_front/domain/entities/schedule_entity.dart';
 import 'package:on_time_front/domain/entities/schedule_with_preparation_entity.dart';
-import 'package:on_time_front/domain/repositories/schedule_repository.dart';
 import 'package:on_time_front/domain/use-cases/get_preparation_by_schedule_id_use_case.dart';
 import 'package:on_time_front/domain/use-cases/get_schedules_by_date_use_case.dart';
+import 'package:on_time_front/domain/use-cases/load_schedules_for_week_use_case.dart';
 
 @Injectable()
 class GetNearestUpcomingScheduleUseCase {
   final GetSchedulesByDateUseCase _getScheduleByDateUseCase;
   final GetPreparationByScheduleIdUseCase _getPreparationByScheduleIdUseCase;
+  final LoadSchedulesForWeekUseCase _loadSchedulesForWeekUseCase;
 
   GetNearestUpcomingScheduleUseCase(
-      this._getScheduleByDateUseCase, this._getPreparationByScheduleIdUseCase);
+      this._getScheduleByDateUseCase,
+      this._getPreparationByScheduleIdUseCase,
+      this._loadSchedulesForWeekUseCase);
 
-  Stream<ScheduleWithPreparationEntity?> call() {
+  Stream<ScheduleWithPreparationEntity?> call() async* {
     final DateTime now = DateTime.now();
-    final StreamController<ScheduleWithPreparationEntity?> controller =
-        StreamController<ScheduleWithPreparationEntity?>();
 
-    _getScheduleByDateUseCase(now, now.add(const Duration(days: 2)))
-        .listen((schedules) async {
-      if (schedules.isNotEmpty) {
+    _loadSchedulesForWeekUseCase(now);
+
+    final upcomingScheduleStream = _getScheduleByDateUseCase(
+        now.subtract(const Duration(days: 1)),
+        now.add(const Duration(days: 2)));
+    await for (final upcomingSchedule in upcomingScheduleStream) {
+      if (upcomingSchedule.isNotEmpty) {
         final preparation =
-            await _getPreparationByScheduleIdUseCase(schedules.first.id);
+            await _getPreparationByScheduleIdUseCase(upcomingSchedule.first.id);
         final scheduleWithPreparation =
             ScheduleWithPreparationEntity.fromScheduleAndPreparationEntity(
-                schedules.first, preparation);
-        controller.add(scheduleWithPreparation);
+                upcomingSchedule.first, preparation);
+        yield scheduleWithPreparation;
       } else {
-        controller.add(null);
+        yield null;
       }
-    });
-
-    return controller.stream;
+    }
   }
 }

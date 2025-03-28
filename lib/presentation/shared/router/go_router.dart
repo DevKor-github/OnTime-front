1667537af0ef350fc2d587660496897dca20566a
jjoonleo <jjoonleo@gmail.com>
2025-03-28 17:05:46 +0900

    feat: integrate NavigationService and update GoRouter configuration

--- lib/presentation/shared/router/go_router.dart
+++ lib/presentation/shared/router/go_router.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import 'package:on_time_front/core/di/di_setup.dart';
+import 'package:on_time_front/core/services/navigation_service.dart';
 import 'package:on_time_front/domain/entities/preparation_entity.dart';
-import 'package:on_time_front/presentation/alarm/screeens/alarm_screen.dart';
 import 'package:on_time_front/domain/entities/schedule_entity.dart';
 import 'package:on_time_front/presentation/alarm/screens/alarm_screen.dart';
 import 'package:on_time_front/presentation/alarm/screens/schedule_start_screen.dart';
@@ -18,10 +19,12 @@ import 'package:on_time_front/presentation/schedule_create/screens/schedule_edit
 import 'package:on_time_front/presentation/shared/components/bottom_nav_bar_scaffold.dart';
 import 'package:on_time_front/presentation/shared/utils/stream_to_listenable.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+
 GoRouter goRouterConfig(AppBloc bloc) {
-  dynamic extra;
   return GoRouter(
     refreshListenable: StreamToListenable([bloc.stream]),
+    navigatorKey: getIt.get<NavigationService>().navigatorKey,
     redirect: (BuildContext context, GoRouterState state) {
       final status = bloc.state.status;
       final bool onSignInScreen = state.fullPath == '/signIn';
@@ -45,10 +48,6 @@ GoRouter goRouterConfig(AppBloc bloc) {
             return '/onboarding/start';
           }
         case AppStatus.preparationStarted:
-          extra = bloc.state.schedule!;
-          //bloc.add(AppPreparationStarted());
-          return '/alarmScreen';
-        case AppStatus.preparationOnGoing:
           return null;
       }
     },
@@ -92,16 +91,16 @@ GoRouter goRouterConfig(AppBloc bloc) {
               preparationEntity: state.extra as PreparationEntity)),
       GoRoute(
         path: '/scheduleStart',
+        name: 'scheduleStart',
         builder: (context, state) {
-          final schedule = state.extra as ScheduleEntity;
-          return ScheduleStart(schedule: schedule);
+          return ScheduleStart(schedule: state.extra as ScheduleEntity);
         },
       ),
       GoRoute(
         path: '/alarmScreen',
         builder: (context, state) {
-          // final schedule = state.extra as ScheduleEntity;
-          return AlarmScreen(schedule: extra);
+          final schedule = state.extra as ScheduleEntity;
+          return AlarmScreen(schedule: schedule);
         },
       ),
       GoRoute(

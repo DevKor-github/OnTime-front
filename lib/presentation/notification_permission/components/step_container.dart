f8ca6aff2f360dec0e3a2879ab416e308072d7d0
jjoonleo <jjoonleo@gmail.com>
2025-04-07 17:23:59 +0900

    feat: add StepContainer widget for structured notification permission steps

---
+++ lib/presentation/notification_permission/components/step_container.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class StepContainer extends StatelessWidget {
+  const StepContainer({
+    super.key,
+    required this.step,
+    required this.title,
+    required this.image,
+  });
+
+  final int step;
+  final RichText title;
+  final Widget image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 242, 242, 247),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(23),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _Title(step: step, title: title),
+            const SizedBox(height: 15),
+            image,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({
+    required this.step,
+    required this.title,
+  });
+
+  final int step;
+  final RichText title;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'Step $step',
+          style: textTheme.titleLarge?.copyWith(
+            color: colorScheme.outlineVariant,
+          ),
+        ),
+        const SizedBox(height: 2),
+        title,
+      ],
+    );
+  }
+}

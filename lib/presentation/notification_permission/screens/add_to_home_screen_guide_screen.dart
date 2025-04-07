c4da8ea8204595683e5ac109c6c7c2f3db06a5e4
jjoonleo <jjoonleo@gmail.com>
2025-04-07 18:04:42 +0900

    refactor: enhance AddToHomeScreenGuideScreen layout with scrollable content and improved structure

--- lib/presentation/notification_permission/screens/add_to_home_screen_guide_screen.dart
+++ lib/presentation/notification_permission/screens/add_to_home_screen_guide_screen.dart
@@ -8,17 +8,21 @@ class AddToHomeScreenGuideScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              _TitleContainer(),
-              SizedBox(
-                height: 35,
-              ),
-              _StepListContainer()
-            ],
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 78.0) +
+                EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                _TitleContainer(),
+                SizedBox(
+                  height: 35,
+                ),
+                _StepListContainer()
+              ],
+            ),
           ),
         ),
       ),
@@ -76,33 +80,75 @@ class _StepListContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        _Step1Container(),
+        SizedBox(height: 28),
+        _Step2Container(),
+      ],
+    );
+  }
+}
+
+class _Step1Container extends StatelessWidget {
+  const _Step1Container();
+
+  @override
+  Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
-    return Column(
-      children: [
-        StepContainer(
-          step: 1,
-          title: RichText(
-            text: TextSpan(
-              text: '화면 하단의 ',
-              style: textTheme.titleLarge,
-              children: [
-                TextSpan(
-                  text: '[공유버튼]',
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        color: colorScheme.primary,
-                      ),
-                ),
-                TextSpan(text: ' 클릭'),
-              ],
+    return StepContainer(
+      step: 1,
+      title: RichText(
+        text: TextSpan(
+          text: '화면 하단의 ',
+          style: textTheme.titleLarge,
+          children: [
+            TextSpan(
+              text: '[공유버튼]',
+              style: textTheme.titleLarge?.copyWith(
+                color: colorScheme.primary,
+              ),
             ),
-          ),
-          image: Image.asset(
-            'guides/share_button_guide.png',
-            package: 'assets',
-          ),
+            TextSpan(text: ' 클릭'),
+          ],
         ),
-      ],
+      ),
+      image: Image.asset(
+        'guides/share_button_guide.png',
+        package: 'assets',
+      ),
+    );
+  }
+}
+
+class _Step2Container extends StatelessWidget {
+  const _Step2Container();
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return StepContainer(
+      step: 2,
+      title: RichText(
+        text: TextSpan(
+          text: '홈화면에 추가하기',
+          style: textTheme.titleLarge?.copyWith(
+            color: colorScheme.primary,
+          ),
+          children: [
+            TextSpan(
+              text: '찾아서 선택',
+              style: textTheme.titleLarge,
+            ),
+          ],
+        ),
+      ),
+      image: Image.asset(
+        'guides/add_to_home_screen_guide.png',
+        package: 'assets',
+      ),
     );
   }
 }

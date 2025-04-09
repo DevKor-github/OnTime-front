c6e3857d824171f581c13614bb6afe00c79bb02e
SeoHyeonSim <90302917+SeoHyeonSim@users.noreply.github.com>
2025-04-09 23:37:31 +0900

    refactor: change modal component according to alert dialog style

--- widgetbook/lib/modal_component.dart
+++ widgetbook/lib/modal_component.dart
@@ -1,91 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:on_time_front/presentation/shared/components/modal_component.dart';
+import 'package:on_time_front/presentation/shared/components/modal_button.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 import 'package:on_time_front/presentation/shared/theme/theme.dart';
 
 @widgetbook.UseCase(
-  name: 'Modal Default',
+  name: 'Dynamic Buttons',
   type: ModalComponent,
 )
 Widget useCaseModalComponent(BuildContext context) {
-  final String modalTitleText = context.knobs.string(
-    label: 'Modal Title Text',
-    initialValue: 'Title Text',
+  final titleText = context.knobs.string(
+    label: 'Modal Title',
+    initialValue: 'Modal Title',
   );
 
-  final String modalDetailText = context.knobs.string(
-    label: 'Modal Detail Text',
-    initialValue: 'Detail Text',
+  final contentText = context.knobs.string(
+    label: 'Modal Detail',
+    initialValue: 'Modal Detail',
   );
 
-  final String leftButtonText = context.knobs.string(
-    label: 'Left Button Text',
-    initialValue: 'Left',
+  final showFirstButton = context.knobs.boolean(
+    label: 'First Button',
+    initialValue: true,
   );
 
-  final String rightButtonText = context.knobs.string(
-    label: 'Right Button Text',
-    initialValue: 'Right',
+  final firstButtonText = context.knobs.string(
+    label: 'Button Text',
+    initialValue: 'Button',
   );
 
-  final Color leftButtonColor = context.knobs.list<Color>(
-    label: 'Left Button Color',
-    initialOption: colorScheme.surfaceContainerLow,
-    options: [
-      colorScheme.primary,
-      colorScheme.secondary,
-      colorScheme.surfaceContainerLow,
-      colorScheme.error,
-    ],
-  );
+  final List<Widget> buttons = [];
 
-  final Color leftButtonTextColor = context.knobs.list<Color>(
-    label: 'Left Button Text Color',
-    options: [
-      colorScheme.onPrimary,
-      colorScheme.onSecondary,
-      colorScheme.onSurfaceVariant,
-      colorScheme.onError,
-    ],
-  );
-
-  final Color rightButtonColor = context.knobs.list<Color>(
-    label: 'Right Button Color',
-    options: [
-      colorScheme.primary,
-      colorScheme.secondary,
-      colorScheme.surfaceContainerLow,
-      colorScheme.error,
-    ],
-  );
-
-  final Color rightButtonTextColor = context.knobs.list<Color>(
-    label: 'Right Button Text Color',
-    options: [
-      colorScheme.onPrimary,
-      colorScheme.onSecondary,
-      colorScheme.onSurfaceVariant,
-      colorScheme.onError,
-    ],
-  );
+  if (showFirstButton) {
+    buttons.add(
+      ModalButton(
+        onPressed: () {},
+        text: firstButtonText,
+        color: colorScheme.surfaceContainer,
+        textColor: colorScheme.outline,
+      ),
+    );
+  }
 
   return Scaffold(
     backgroundColor: const Color.fromARGB(255, 243, 241, 241),
     body: Center(
-      child: SizedBox(
-        child: ModalComponent(
-          leftPressed: () {},
-          rightPressed: () {},
-          modalTitleText: modalTitleText,
-          modalDetailText: modalDetailText,
-          leftButtonText: leftButtonText,
-          rightButtonText: rightButtonText,
-          leftButtonColor: leftButtonColor,
-          leftButtonTextColor: leftButtonTextColor,
-          rightButtonColor: rightButtonColor,
-          rightButtonTextColor: rightButtonTextColor,
-        ),
+      child: ModalComponent(
+        title: Text(titleText),
+        content: Text(contentText),
+        actions: buttons,
       ),
     ),
   );

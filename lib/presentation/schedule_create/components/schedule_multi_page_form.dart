716bd4f0fb0f2f6f63f208d0e625581c0b47c666
SeoHyeonSim <90302917+SeoHyeonSim@users.noreply.github.com>
2025-06-01 18:32:19 +0900

    refactor: replace route changing for home screen to closing modal form

--- lib/presentation/schedule_create/components/schedule_multi_page_form.dart
+++ lib/presentation/schedule_create/components/schedule_multi_page_form.dart
@@ -131,7 +131,8 @@ class _ScheduleMultiPageFormState extends State<ScheduleMultiPageForm>
       _updateCurrentPageIndex(_tabController.index + 1);
     } else {
       widget.onSaved?.call();
-      context.go('/home');
+      // context.go('/home');
+      Navigator.of(context).pop(); // Close the form
     }
   }
 
@@ -139,7 +140,8 @@ class _ScheduleMultiPageFormState extends State<ScheduleMultiPageForm>
     if (_tabController.index > 0) {
       _updateCurrentPageIndex(_tabController.index - 1);
     } else {
-      context.go('/home');
+      // context.go('/home');
+      Navigator.of(context).pop(); // Close the form
     }
   }
 

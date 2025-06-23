0ecd4b440fcb8c7925efc7ec58e64d9efeccb599
jjoonleo <jjoonleo@gmail.com>
2025-06-23 18:08:49 +0900

    refactor: remove unused import from modal_button.dart

--- widgetbook/lib/modal_button.dart
+++ widgetbook/lib/modal_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:on_time_front/presentation/shared/components/modal_button.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
-import 'package:on_time_front/presentation/shared/theme/theme.dart';
 
 @widgetbook.UseCase(
   name: 'Default',

53c30c615931571be3a512dcaed5a8e9e1b85cb7
jjoonleo <jjoonleo@gmail.com>
2025-07-07 15:14:30 +0900

    feat: restructure theme management by separating color scheme and text theme
    - Introduced a new color_scheme.dart file to define the color scheme.
    - Removed the custom text theme file and integrated its functionality into the main text_theme.dart.
    - Updated imports to streamline theme-related references and ensure consistency across the theme files.

--- lib/presentation/shared/theme/theme.dart
+++ lib/presentation/shared/theme/theme.dart
@@ -3,41 +3,10 @@ import 'package:on_time_front/presentation/home/components/week_calendar.dart';
 import 'package:on_time_front/presentation/shared/constants/app_colors.dart';
 import 'package:on_time_front/presentation/shared/theme/tile_style.dart';
 import 'package:on_time_front/presentation/shared/theme/button_styles.dart';
-import 'package:on_time_front/presentation/shared/theme/text_theme.dart';
 import 'package:on_time_front/presentation/shared/theme/input_decoration_theme.dart';
 
-ColorScheme _colorScheme = ColorScheme(
-  brightness: Brightness.light,
-  primary: AppColors.blue,
-  onPrimary: AppColors.white,
-  primaryContainer: AppColors.blue.shade200,
-  onPrimaryContainer: AppColors.blue.shade900,
-  secondary: AppColors.green,
-  onSecondary: AppColors.white,
-  secondaryContainer: AppColors.green.shade100,
-  onSecondaryContainer: AppColors.green.shade900,
-  tertiary: AppColors.yellow,
-  onTertiary: AppColors.white,
-  tertiaryContainer: AppColors.yellow.shade100,
-  onTertiaryContainer: AppColors.yellow.shade900,
-  error: AppColors.red,
-  onError: Colors.white,
-  errorContainer: AppColors.red.shade50,
-  onErrorContainer: AppColors.red.shade900,
-  surfaceDim: AppColors.grey.shade400,
-  surface: AppColors.white,
-  surfaceBright: AppColors.white,
-  surfaceContainerLowest: AppColors.grey.shade50,
-  surfaceContainerLow: AppColors.grey.shade100,
-  surfaceContainer: AppColors.grey.shade200,
-  surfaceContainerHigh: AppColors.grey.shade300,
-  surfaceContainerHighest: AppColors.grey.shade400,
-  onSurface: AppColors.grey[950]!,
-  outlineVariant: AppColors.grey.shade400,
-  outline: AppColors.grey.shade600,
-);
-
-TextTheme _textTheme = AppTextTheme.create(_colorScheme);
+part 'text_theme.dart';
+part 'color_scheme.dart';
 
 ThemeData themeData = ThemeData(
   colorScheme: _colorScheme,

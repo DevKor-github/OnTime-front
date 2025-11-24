b70d4b56235cd597e8657b326a992e506298cf02
jjoonleo <jjoonleo@gmail.com>
2025-11-24 21:16:01 +0900

    feat: implement LoadSchedulesByDateUseCase and refactor schedule loading logic
    - Added LoadSchedulesByDateUseCase to fetch schedules for a specified date range, improving modularity and reusability.
    - Refactored LoadSchedulesForMonthUseCase and LoadSchedulesForWeekUseCase to utilize LoadSchedulesByDateUseCase, streamlining schedule retrieval logic.

--- lib/domain/use-cases/load_schedules_for_month_use_case.dart
+++ lib/domain/use-cases/load_schedules_for_month_use_case.dart
@@ -1,16 +1,16 @@
 import 'package:injectable/injectable.dart';
-import 'package:on_time_front/domain/repositories/schedule_repository.dart';
+import 'package:on_time_front/domain/use-cases/load_schedules_by_date_use_case.dart';
 
 @Injectable()
 class LoadSchedulesForMonthUseCase {
-  final ScheduleRepository _scheduleRepository;
+  final LoadSchedulesByDateUseCase _loadSchedulesByDateUseCase;
 
-  LoadSchedulesForMonthUseCase(this._scheduleRepository);
+  LoadSchedulesForMonthUseCase(this._loadSchedulesByDateUseCase);
 
   Future<void> call(DateTime date) async {
     final startOfMonth = DateTime(date.year, date.month, 1);
     final endOfMonth = DateTime(date.year, date.month + 1, 0);
 
-    await _scheduleRepository.getSchedulesByDate(startOfMonth, endOfMonth);
+    await _loadSchedulesByDateUseCase(startOfMonth, endOfMonth);
   }
 }
